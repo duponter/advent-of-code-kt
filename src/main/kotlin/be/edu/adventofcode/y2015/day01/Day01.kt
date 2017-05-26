3c80b556ef3f75afc7f3627552b0a52c110e78cf
Erwin Dupont <erwin.dupont@aca-it.be>
2017-05-26 23:13:52 +0200

    [2015 - Day 01] Implemented part 1 and 2

--- src/main/kotlin/be/edu/adventofcode/y2015/day01/Day01.kt
+++ src/main/kotlin/be/edu/adventofcode/y2015/day01/Day01.kt
@@ -1,7 +1,24 @@
 package be.edu.adventofcode.y2015.day01
 
-fun start(): String = "Advent of Code, 2015, Day 01"
+import java.io.File
 
-fun main(args: Array<String>) {
-    println(start())
+class Day01 {
+    fun part1(): Int {
+        val input = File(this.javaClass.getResource("Day01.txt").toURI()).readText()
+        return input.fold(0, { pos, next -> pos + if (next == '(') 1 else -1 })
+    }
+
+    fun part2(): Int {
+        val input = File(this.javaClass.getResource("Day01.txt").toURI()).readText()
+        return input.asIterable()
+                .scanLeft(0, { pos, next -> pos + if (next == '(') 1 else -1 })
+                .takeWhile({ it != -1 })
+                .size
+    }
+
+    fun <T : Any, R : Any> Iterable<T>.scanLeft(initial: R, operation: (R, T) -> R): List<R> {
+        val result = arrayListOf(initial)
+        forEach { result.add(operation(result.last(), it)) }
+        return result
+    }
 }
---
+++ src/test/kotlin/be/edu/adventofcode/y2015/day01/Day01Spec.kt
@@ -0,0 +1,27 @@
+package be.edu.adventofcode.y2015.day01
+
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertEquals
+
+object Day01Spec : Spek({
+    given("day 01") {
+        val day01 = Day01();
+        on("part 1") {
+            val floor = day01.part1()
+            it("should answer To what floor do the instructions take Santa?") {
+                assertEquals(232, floor)
+            }
+        }
+        on("part 2") {
+            val position = day01.part2()
+            it("should answer What is the position of the character that causes Santa to first enter the basement?") {
+                assertEquals(1783, position)
+            }
+        }
+    }
+})
+
+
