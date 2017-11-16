729d3645e26ca9f87f2fe6a60505e5c90416dfb1
Erwin Dupont <erwin.dupont@aca-it.be>
2017-11-16 21:08:08 +0100

    [2015 - Day 04] Implementation solution for part 1 and 2

--- src/main/kotlin/be/edu/adventofcode/y2015/day04/Day04.kt
+++ src/main/kotlin/be/edu/adventofcode/y2015/day04/Day04.kt
@@ -1,13 +1,31 @@
 package be.edu.adventofcode.y2015.day04
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
+import java.security.MessageDigest
 
 class Day04 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Text): Int {
+        val md = MessageDigest.getInstance("MD5")
+        return generateSequence(1, { it + 1 })
+                .map { Pair(it, String.format("%s%d", input.get(), it).hash(md)) }
+                .dropWhile { !it.second.startsWith("00000") }
+                .first()
+                .first
     }
 
-    fun part2(input: Lines): Int {
-        return input.get().count()
+    fun part2(input: Text): Int {
+        val md = MessageDigest.getInstance("MD5")
+        return generateSequence(1, { it + 1 })
+                .map { Pair(it, String.format("%s%d", input.get(), it).hash(md)) }
+                .dropWhile { !it.second.startsWith("000000") }
+                .first()
+                .first
+    }
+
+    fun String.hash(messageDigest: MessageDigest): String {
+        val digested = messageDigest.digest(toByteArray())
+        return digested.joinToString("") {
+            String.format("%02x", it)
+        }
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2015/day04/Day04Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2015/day04/Day04Spec.kt
@@ -1,30 +1,29 @@
 package be.edu.adventofcode.y2015.day04
 
-import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
-import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.dsl.xit
 import kotlin.test.assertEquals
 
 object Day04Spec : Spek({
     given("day 04") {
         val day04 = Day04();
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day04.part1(LinesFromArray("line")))
+            xit("If your secret key is abcdef, the lowest positive number is 609043") {
+                assertEquals(609043, day04.part1(TextFromString("abcdef")))
             }
-            it("should answer Part1") {
-                assertEquals(0, day04.part1(DayInput().lines(day04)))
+            xit("If your secret key is pqrstuv, the lowest positive number is 1048970") {
+                assertEquals(1048970, day04.part1(TextFromString("pqrstuv")))
+            }
+            xit("should answer What is the lowest positive number that produces a hash starting with 5+ leading zeros?") {
+                assertEquals(254575, day04.part1(TextFromString("bgvyzdsv")))
             }
         }
         on("part 2") {
-            it("testcase") {
-                assertEquals(1, day04.part2(LinesFromArray("line")))
-            }
-            it("should answer Part2") {
-                assertEquals(0, day04.part2(DayInput().lines(day04)))
+            xit("should answer What is the lowest positive number that produces a hash starting with 6+ leading zeros?") {
+                assertEquals(1038736, day04.part2(TextFromString("bgvyzdsv")))
             }
         }
     }
