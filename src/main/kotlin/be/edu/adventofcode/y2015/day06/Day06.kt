32054622d421d9d293bb0ad505c639b372b54f4a
Erwin Dupont <erwin.dupont@aca-it.be>
2017-11-22 17:33:43 +0100

    [2015 - Day 06] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2015/day06/Day06.kt
+++ src/main/kotlin/be/edu/adventofcode/y2015/day06/Day06.kt
@@ -4,10 +4,63 @@ import be.edu.adventofcode.Lines
 
 class Day06 {
     fun part1(input: Lines): Int {
-        return input.get().count()
+        val grid = Grid()
+        input.get().mapNotNull { toInstruction(it) }.forEach({ it.applyTo(grid) })
+        return grid.countLit()
     }
 
     fun part2(input: Lines): Int {
         return input.get().count()
     }
+
+    private fun toInstruction(input: String): Instruction? = Instruction.REGEX.matchEntire(input)?.destructured?.let { destructured ->
+        val upperLeft = Coordinates(destructured.component2().toInt(), destructured.component3().toInt())
+        val lowerRight = Coordinates(destructured.component4().toInt(), destructured.component5().toInt())
+        return when (destructured.component1()) {
+            "turn on" -> Instruction(TurnOn(), upperLeft, lowerRight)
+            "toggle" -> Instruction(Toggle(), upperLeft, lowerRight)
+            "turn off" -> Instruction(TurnOff(), upperLeft, lowerRight)
+            else -> null
+        }
+    }
+}
+
+class Grid(rows: Int, private val cols: Int) {
+    private val grid = Array(rows, { Array<Boolean>(cols, { false }) })
+
+    constructor() : this(1000, 1000)
+
+    fun apply(row: Int, col: Int, operation: Operation) {
+        grid[row][col] = operation.apply(grid[row][col])
+    }
+
+    fun countLit(): Int = grid.flatMap { it -> it.toList() }.filter { it }.count()
+}
+
+data class Coordinates(val row: Int, val col: Int)
+
+data class Instruction(private val operation: Operation, private val upperLeft: Coordinates, private val lowerRight: Coordinates) {
+    companion object {
+        val REGEX = Regex("([\\w ]+) (\\d+),(\\d+) through (\\d+),(\\d+)")
+    }
+
+    fun applyTo(grid: Grid) {
+        (upperLeft.row..lowerRight.row).forEach { row -> (upperLeft.col..lowerRight.col).forEach { col -> grid.apply(row, col, operation) } }
+    }
+}
+
+sealed class Operation {
+    abstract fun apply(current: Boolean): Boolean
+}
+
+class TurnOn : Operation() {
+    override fun apply(current: Boolean): Boolean = true
+}
+
+class TurnOff : Operation() {
+    override fun apply(current: Boolean): Boolean = false
+}
+
+class Toggle : Operation() {
+    override fun apply(current: Boolean): Boolean = !current
 }
--- src/test/kotlin/be/edu/adventofcode/y2015/day06/Day06Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2015/day06/Day06Spec.kt
@@ -13,16 +13,16 @@ object Day06Spec : Spek({
         val day06 = Day06();
         on("part 1") {
             it("turn on 0,0 through 999,999 would turn on (or leave on) every light") {
-                assertEquals(1, day06.part1(LinesFromArray("turn on 0,0 through 999,999")))
+                assertEquals(1000000, day06.part1(LinesFromArray("turn on 0,0 through 999,999")))
             }
             it("toggle 0,0 through 999,0 would toggle the first line of 1000 lights") {
-                assertEquals(1, day06.part1(LinesFromArray("toggle 0,0 through 999,0")))
+                assertEquals(1000, day06.part1(LinesFromArray("toggle 0,0 through 999,0")))
             }
             it("turn off 499,499 through 500,500 would turn off (or leave off) the middle four lights") {
-                assertEquals(1, day06.part1(LinesFromArray("turn off 499,499 through 500,500")))
+                assertEquals(999996, day06.part1(LinesFromArray("turn on 0,0 through 999,999", "turn off 499,499 through 500,500")))
             }
             it("should answer How many lights are lit?") {
-                assertEquals(0, day06.part1(DayInput().lines(day06)))
+                assertEquals(400410, day06.part1(DayInput().lines(day06)))
             }
         }
         on("part 2") {
