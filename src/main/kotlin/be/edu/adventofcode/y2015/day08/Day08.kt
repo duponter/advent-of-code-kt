0940231f490ce4de5a2a35cf3d607ed6fefbb0f9
Erwin Dupont <erwin.dupont@aca-it.be>
2017-11-24 13:15:26 +0100

    [2015 - Day 08] Generation

---
+++ src/main/kotlin/be/edu/adventofcode/y2015/day08/Day08.kt
@@ -0,0 +1,13 @@
+package be.edu.adventofcode.y2015.day08
+
+import be.edu.adventofcode.Lines
+
+class Day08 {
+    fun part1(input: Lines): Int {
+        return input.get().count()
+    }
+
+    fun part2(input: Lines): Int {
+        return input.get().count()
+    }
+}
---
+++ src/test/kotlin/be/edu/adventofcode/y2015/day08/Day08Spec.kt
@@ -0,0 +1,31 @@
+package be.edu.adventofcode.y2015.day08
+
+import be.edu.adventofcode.DayInput
+import be.edu.adventofcode.LinesFromArray
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertEquals
+
+object Day08Spec : Spek({
+    given("day 08") {
+        val day08 = Day08();
+        on("part 1") {
+            it("testcase") {
+                assertEquals(1, day08.part1(LinesFromArray("line")))
+            }
+            it("should answer Part1") {
+                assertEquals(0, day08.part1(DayInput().lines(day08)))
+            }
+        }
+        on("part 2") {
+            it("testcase") {
+                assertEquals(1, day08.part2(LinesFromArray("line")))
+            }
+            it("should answer Part2") {
+                assertEquals(0, day08.part2(DayInput().lines(day08)))
+            }
+        }
+    }
+})
