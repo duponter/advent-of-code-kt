03d0fbff621c94851edcd3e2e65b3f28c98b934f
Erwin Dupont <erwin.dupont@aca-it.be>
2017-11-28 17:15:27 +0100

    [2015 - Day 10] Solved part 1 and 2

--- src/main/kotlin/be/edu/adventofcode/y2015/day10/Day10.kt
+++ src/main/kotlin/be/edu/adventofcode/y2015/day10/Day10.kt
@@ -1,13 +1,14 @@
 package be.edu.adventofcode.y2015.day10
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day10 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
-    }
+    fun part1(input: Text): Int = recurse(40, this::lookAndSay)(input.get()).length
 
-    fun part2(input: Lines): Int {
-        return input.get().count()
-    }
+    fun part2(input: Text): Int = recurse(50, this::lookAndSay)(input.get()).length
+
+    private fun <T> recurse(times: Int, function: (T) -> T): (T) -> T = if (times < 2) function else { x -> recurse(times - 1, function)(function(x)) }
+
+    fun lookAndSay(input: String): String = input.split(Regex("(?<=(.))(?!\\1)")).dropLastWhile { it.isEmpty() }
+            .joinToString("") { it.length.toString().plus(it.first()) }
 }
--- src/test/kotlin/be/edu/adventofcode/y2015/day10/Day10Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2015/day10/Day10Spec.kt
@@ -1,7 +1,6 @@
 package be.edu.adventofcode.y2015.day10
 
-import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -10,21 +9,30 @@ import kotlin.test.assertEquals
 
 object Day10Spec : Spek({
     given("day 10") {
-        val day10 = Day10();
+        val day10 = Day10()
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day10.part1(LinesFromArray("line")))
+            it("1 becomes 11 (1 copy of digit 1).") {
+                assertEquals("11", day10.lookAndSay("1"))
             }
-            it("should answer Part1") {
-                assertEquals(0, day10.part1(DayInput().lines(day10)))
+            it("11 becomes 21 (2 copies of digit 1).") {
+                assertEquals("21", day10.lookAndSay("11"))
+            }
+            it("21 becomes 1211 (one 2 followed by one 1).") {
+                assertEquals("1211", day10.lookAndSay("21"))
+            }
+            it("1211 becomes 111221 (one 1, one 2, and two 1s).") {
+                assertEquals("111221", day10.lookAndSay("1211"))
+            }
+            it("111221 becomes 312211 (three 1s, two 2s, and one 1).") {
+                assertEquals("312211", day10.lookAndSay("111221"))
+            }
+            it("should answer Apply look-and-say 40 times. What is the length of the result?") {
+                assertEquals(252594, day10.part1(TextFromString("1113222113")))
             }
         }
         on("part 2") {
-            it("testcase") {
-                assertEquals(1, day10.part2(LinesFromArray("line")))
-            }
-            it("should answer Part2") {
-                assertEquals(0, day10.part2(DayInput().lines(day10)))
+            it("should answer Apply look-and-say 40 times. What is the length of the result?") {
+                assertEquals(3579328, day10.part2(TextFromString("1113222113")))
             }
         }
     }
