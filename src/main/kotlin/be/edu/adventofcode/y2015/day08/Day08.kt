89f09a88fa41a8b853478860fbc19c865b25f50c
Erwin Dupont <erwin.dupont@aca-it.be>
2017-11-28 09:07:20 +0100

    [2015 - Day 08] Initial implementation part 1

--- src/main/kotlin/be/edu/adventofcode/y2015/day08/Day08.kt
+++ src/main/kotlin/be/edu/adventofcode/y2015/day08/Day08.kt
@@ -4,10 +4,29 @@ import be.edu.adventofcode.Lines
 
 class Day08 {
     fun part1(input: Lines): Int {
-        return input.get().count()
+        return input.get().map { Pair(it.length, it.replace("\\\"", "\"").replace("\\\\", "\\").replace(Regex("\\\\[xX]([0-9a-fA-F]+)"), { String(it.destructured.component1().hexStringToByteArray()) }).length) }
+                .map { it.first - it.second }
+                .sum()
     }
 
     fun part2(input: Lines): Int {
         return input.get().count()
     }
+}
+
+private const val HEX_CHARS = "0123456789ABCDEF"
+
+fun String.hexStringToByteArray(): ByteArray {
+
+    val result = ByteArray(length / 2)
+
+    for (i in 0 until length step 2) {
+        val firstIndex = HEX_CHARS.indexOf(this[i]);
+        val secondIndex = HEX_CHARS.indexOf(this[i + 1]);
+
+        val octet = firstIndex.shl(4).or(secondIndex)
+        result[i.shr(1)] = octet.toByte()
+    }
+
+    return result
 }
--- src/test/kotlin/be/edu/adventofcode/y2015/day08/Day08Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2015/day08/Day08Spec.kt
@@ -12,10 +12,20 @@ object Day08Spec : Spek({
     given("day 08") {
         val day08 = Day08();
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day08.part1(LinesFromArray("line")))
+
+            it("\"\" is 2 characters of code (the two double quotes), but the string contains zero characters.") {
+                assertEquals(2, day08.part1(LinesFromArray("\"\"")))
             }
-            it("should answer Part1") {
+            it("\"abc\" is 5 characters of code, but 3 characters in the string data.") {
+                assertEquals(2, day08.part1(LinesFromArray("\"abc\"")))
+            }
+            it("\"aaa\\\"aaa\" is 10 characters of code, but the string itself contains six \"a\" characters and a single, escaped quote character, for a total of 7 characters in the string data.") {
+                assertEquals(3, day08.part1(LinesFromArray("\"aaa\\\"aaa\"")))
+            }
+            it("\"\\x27\" is 6 characters of code, but the string itself contains just one - an apostrophe ('), escaped using hexadecimal notation.") {
+                assertEquals(5, day08.part1(LinesFromArray("\"\\x27\"")))
+            }
+            it("should answer What is the number of characters of code for string literals minus the number of characters in memory for the values of the strings?") {
                 assertEquals(0, day08.part1(DayInput().lines(day08)))
             }
         }
