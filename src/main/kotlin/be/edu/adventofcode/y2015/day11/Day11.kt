11dce6734bac0d07a3782c982d5f14fcaca012fc
Erwin Dupont <erwin.dupont@aca-it.be>
2017-11-30 09:50:29 +0100

    [2015 - Day 11] Solved part 1 and 2

--- src/main/kotlin/be/edu/adventofcode/y2015/day11/Day11.kt
+++ src/main/kotlin/be/edu/adventofcode/y2015/day11/Day11.kt
@@ -1,13 +1,41 @@
 package be.edu.adventofcode.y2015.day11
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
+import be.edu.adventofcode.TextFromString
 
 class Day11 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Text): String = generateSequence(input.get(), String::inc).drop(1)
+            .first { meetsRequirements(it) }
+
+    fun part2(input: Text): String {
+        return part1(TextFromString(part1(input)))
     }
 
-    fun part2(input: Lines): Int {
-        return input.get().count()
+    fun meetsRequirements(input: String): Boolean = noConfusingLetters(input) && oneIncreasingLetterTriple(input) && twoLetterPairs(input)
+
+    private fun noConfusingLetters(input: String) = listOf('i', 'o', 'l').none { input.contains(it) }
+
+    private fun oneIncreasingLetterTriple(input: String): Boolean = ('a'..'x').map { "$it${it.inc()}${it.inc().inc()}" }
+            .filter { noConfusingLetters(it) }
+            .any { input.contains(it) }
+
+
+    private fun twoLetterPairs(input: String): Boolean {
+        val atLeastPairs = input.groupSimilar().filter { it.length > 1 }
+        return atLeastPairs.size > 1 || atLeastPairs.firstOrNull()?.length ?: 0 > 3
     }
 }
+
+
+fun String.inc(): String {
+    val last = this.last()
+    val remaining = this.dropLast(1)
+
+    return if (last != 'z') {
+        remaining.plus(last.inc())
+    } else {
+        remaining.inc().plus('a')
+    }
+}
+
+fun String.groupSimilar(): List<String> = this.split(Regex("(?<=(.))(?!\\1)")).dropLastWhile { it.isEmpty() }
--- src/test/kotlin/be/edu/adventofcode/y2015/day11/Day11Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2015/day11/Day11Spec.kt
@@ -1,7 +1,6 @@
 package be.edu.adventofcode.y2015.day11
 
-import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -12,19 +11,28 @@ object Day11Spec : Spek({
     given("day 11") {
         val day11 = Day11()
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day11.part1(LinesFromArray("line")))
+            it("hijklmmn meets the first requirement but fails the second requirement requirement") {
+                assertEquals(false, day11.meetsRequirements(TextFromString("hijklmmn").get()))
             }
-            it("should answer Part1") {
-                assertEquals(0, day11.part1(DayInput().lines(day11)))
+            it("abbceffg meets the third requirement but fails the first requirement.") {
+                assertEquals(false, day11.meetsRequirements(TextFromString("abbceffg").get()))
+            }
+            it("abbcegjk fails the third requirement, because it only has one double letter (bb).") {
+                assertEquals(false, day11.meetsRequirements(TextFromString("abbcegjk").get()))
+            }
+            it("The next password after abcdefgh is abcdffaa.") {
+                assertEquals("abcdffaa", day11.part1(TextFromString("abcdefgh")))
+            }
+            it("The next password after ghijklmn is ghjaabcc.") {
+                assertEquals("ghjaabcc", day11.part1(TextFromString("ghijklmn")))
+            }
+            it("should answer What should his next password be?") {
+                assertEquals("hxbxxyzz", day11.part1(TextFromString("hxbxwxba")))
             }
         }
         on("part 2") {
-            it("testcase") {
-                assertEquals(1, day11.part2(LinesFromArray("line")))
-            }
-            it("should answer Part2") {
-                assertEquals(0, day11.part2(DayInput().lines(day11)))
+            it("should answer Santa's password expired again. What's the next one?") {
+                assertEquals("hxcaabcc", day11.part2(TextFromString("hxbxwxba")))
             }
         }
     }
