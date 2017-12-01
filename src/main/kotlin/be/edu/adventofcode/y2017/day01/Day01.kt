99d2b3565b59e6a3e0b042e4deda6d5913c00f54
Erwin Dupont <erwin.dupont@aca-it.be>
2017-12-01 09:26:04 +0100

    [2017 - Day 01] Solved part 1 and 2

--- src/main/kotlin/be/edu/adventofcode/y2017/day01/Day01.kt
+++ src/main/kotlin/be/edu/adventofcode/y2017/day01/Day01.kt
@@ -1,13 +1,23 @@
 package be.edu.adventofcode.y2017.day01
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day01 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Text): Int {
+        val chars = input.chars()
+        return chars.plus(chars.first()).windowed(2)
+                .filter { it.first() == it.last() }
+                .map { it.first().toString().toInt() }
+                .sum()
     }
 
-    fun part2(input: Lines): Int {
-        return input.get().count()
+    fun part2(input: Text): Int {
+        val chars = input.chars()
+        val halves = chars.windowed(chars.size / 2)
+        return chars.zip(halves.last().plus(halves.first()))
+                .filter { it.first == it.second }
+                .map { it.first.toString().toInt() }
+                .sum()
+
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2017/day01/Day01Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2017/day01/Day01Spec.kt
@@ -1,7 +1,7 @@
 package be.edu.adventofcode.y2017.day01
 
 import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -12,19 +12,40 @@ object Day01Spec : Spek({
     given("day 01") {
         val day01 = Day01()
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day01.part1(LinesFromArray("line")))
+            it("1122 produces a sum of 3 (1 + 2)") {
+                assertEquals(3, day01.part1(TextFromString("1122")))
             }
-            it("should answer Part1") {
-                assertEquals(0, day01.part1(DayInput().lines(day01)))
+            it("1111 produces 4 because each digit (all 1) matches the next.") {
+                assertEquals(4, day01.part1(TextFromString("1111")))
+            }
+            it("1234 produces 0 because no digit matches the next.") {
+                assertEquals(0, day01.part1(TextFromString("1234")))
+            }
+            it("91212129 produces 9 because the only digit that matches the next one is the last digit, 9.") {
+                assertEquals(9, day01.part1(TextFromString("91212129")))
+            }
+            it("should answer What is the solution to your captcha?") {
+                assertEquals(1069, day01.part1(DayInput().text(day01)))
             }
         }
         on("part 2") {
-            it("testcase") {
-                assertEquals(1, day01.part2(LinesFromArray("line")))
+            it("1212 produces 6: the list contains 4 items, and all four digits match the digit 2 items ahead.") {
+                assertEquals(6, day01.part2(TextFromString("1212")))
             }
-            it("should answer Part2") {
-                assertEquals(0, day01.part2(DayInput().lines(day01)))
+            it("1221 produces 0, because every comparison is between a 1 and a 2.") {
+                assertEquals(0, day01.part2(TextFromString("1221")))
+            }
+            it("123425 produces 4, because both 2s match each other, but no other digit has a match.") {
+                assertEquals(4, day01.part2(TextFromString("123425")))
+            }
+            it("123123 produces 12.") {
+                assertEquals(12, day01.part2(TextFromString("123123")))
+            }
+            it("12131415 produces 4.") {
+                assertEquals(4, day01.part2(TextFromString("12131415")))
+            }
+            it("should answer What is the solution to your new captcha?") {
+                assertEquals(1268, day01.part2(DayInput().text(day01)))
             }
         }
     }
