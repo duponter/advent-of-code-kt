86a1becc7014136ff11413c93d7cb9adb2a012cf
Erwin Dupont <erwin.dupont@aca-it.be>
2017-12-03 20:34:15 +0100

    [2017 - Day 03] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2017/day03/Day03.kt
+++ src/main/kotlin/be/edu/adventofcode/y2017/day03/Day03.kt
@@ -1,13 +1,73 @@
 package be.edu.adventofcode.y2017.day03
 
-import be.edu.adventofcode.Lines
+import kotlin.math.abs
 
 class Day03 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Int): Int {
+//        println("position for $input")
+        val sqrt = Math.sqrt(input.toDouble())
+        var size = sqrt.toInt()
+        if (size < sqrt) {
+            size++
+        }
+        val squares = (0..size).map { it * it }
+
+        return (2..input).fold(Triple(1, Point(), Direction.RIGHT), { current, i -> next(squares, current, i) }).second.distance(Point())
     }
 
-    fun part2(input: Lines): Int {
-        return input.get().count()
+    private fun next(squares: List<Int>, current: Triple<Int, Point, Direction>, target: Int): Triple<Int, Point, Direction> {
+        val nextPoint = current.third.apply(current.second)
+
+        val pair = squares.zipWithNext().first { it.second >= target && it.first < target }
+        val oneDirection = (pair.second - pair.first - 1) / 2
+        val progress = target - pair.first
+
+//        println("Drawing $target (between $pair) on $nextPoint from $current: Progress = $progress in one direction of $oneDirection")
+        return Triple(target, nextPoint, if (progress == 1 || progress == (oneDirection + 1)) current.third.turn() else current.third)
     }
+
+    fun part2(input: Int): Int {
+        return input
+    }
+}
+
+data class Point(private val x: Int, private val y: Int) {
+    constructor() : this(0, 0)
+
+    fun up(): Point = Point(x + 1, y)
+    fun down(): Point = Point(x - 1, y)
+    fun right(): Point = Point(x, y + 1)
+    fun left(): Point = Point(x, y - 1)
+
+    fun distance(other: Point): Int = abs(this.x - other.x) + abs(this.y - other.y)
+}
+
+enum class Direction {
+    UP {
+        override fun apply(current: Point): Point = current.up()
+
+        override fun turn(): Direction = LEFT
+    },
+
+    DOWN {
+        override fun apply(current: Point): Point = current.down()
+
+        override fun turn(): Direction = RIGHT
+    },
+
+    LEFT {
+        override fun apply(current: Point): Point = current.left()
+
+        override fun turn(): Direction = DOWN
+    },
+
+    RIGHT {
+        override fun apply(current: Point): Point = current.right()
+
+        override fun turn(): Direction = UP
+    };
+
+    abstract fun apply(current: Point): Point
+
+    abstract fun turn(): Direction
 }
--- src/test/kotlin/be/edu/adventofcode/y2017/day03/Day03Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2017/day03/Day03Spec.kt
@@ -1,30 +1,38 @@
 package be.edu.adventofcode.y2017.day03
 
-import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
+import org.jetbrains.spek.api.dsl.xon
 import kotlin.test.assertEquals
 
 object Day03Spec : Spek({
     given("day 03") {
         val day03 = Day03()
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day03.part1(LinesFromArray("line")))
+            it("Data from square 1 is carried 0 steps, since it's at the access port.") {
+                assertEquals(0, day03.part1(1))
             }
-            it("should answer Part1") {
-                assertEquals(0, day03.part1(DayInput().lines(day03)))
+            it("Data from square 12 is carried 3 steps, such as: down, left, left.") {
+                assertEquals(3, day03.part1(12))
+            }
+            it("Data from square 23 is carried only 2 steps: up twice.") {
+                assertEquals(2, day03.part1(23))
+            }
+            it("Data from square 1024 must be carried 31 steps.") {
+                assertEquals(31, day03.part1(1024))
+            }
+            it("should answer How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?") {
+                assertEquals(430, day03.part1(312051))
             }
         }
-        on("part 2") {
+        xon("part 2") {
             it("testcase") {
-                assertEquals(1, day03.part2(LinesFromArray("line")))
+                //                assertEquals(1, day03.part2(TextFromString("line")))
             }
             it("should answer Part2") {
-                assertEquals(0, day03.part2(DayInput().lines(day03)))
+                //                assertEquals(0, day03.part2(DayInput().lines(day03)))
             }
         }
     }
