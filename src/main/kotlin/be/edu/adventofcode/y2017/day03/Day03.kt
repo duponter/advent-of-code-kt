af2c715c48848515b780869b065423712cbc235d
Erwin Dupont <erwin.dupont@aca-it.be>
2017-12-04 11:07:58 +0100

    [2017 - Day 03] Solved part 2

--- src/main/kotlin/be/edu/adventofcode/y2017/day03/Day03.kt
+++ src/main/kotlin/be/edu/adventofcode/y2017/day03/Day03.kt
@@ -4,7 +4,6 @@ import kotlin.math.abs
 
 class Day03 {
     fun part1(input: Int): Int {
-//        println("position for $input")
         val sqrt = Math.sqrt(input.toDouble())
         var size = sqrt.toInt()
         if (size < sqrt) {
@@ -22,12 +21,30 @@ class Day03 {
         val oneDirection = (pair.second - pair.first - 1) / 2
         val progress = target - pair.first
 
-//        println("Drawing $target (between $pair) on $nextPoint from $current: Progress = $progress in one direction of $oneDirection")
         return Triple(target, nextPoint, if (progress == 1 || progress == (oneDirection + 1)) current.third.turn() else current.third)
     }
 
     fun part2(input: Int): Int {
-        return input
+        // max 4 values: vorige value + 3 van rij eronder/erboven/ernaast
+        val sqrt = Math.sqrt(input.toDouble())
+        var size = sqrt.toInt()
+        if (size < 5) {
+            size = 5
+        } else if (size < sqrt) {
+            size++
+        }
+        val squares = (0..size).map { it * it }
+        val saved = mutableMapOf(Pair(Point(), 1))
+
+        var current = Triple(1, Point(), Direction.RIGHT)
+        var value = 1
+
+        while (value <= input) {
+            current = next(squares, current, current.first + 1)
+            value = current.second.surroundingSum(saved, current.third)
+        }
+
+        return value
     }
 }
 
@@ -40,6 +57,17 @@ data class Point(private val x: Int, private val y: Int) {
     fun left(): Point = Point(x, y - 1)
 
     fun distance(other: Point): Int = abs(this.x - other.x) + abs(this.y - other.y)
+
+    fun surroundingSum(values: MutableMap<Point, Int>, direction: Direction): Int {
+        val pointN = direction.back().apply(this)
+        val pointW = direction.turn().apply(this)
+        val pointNW = direction.turn().turn().apply(pointW)
+        val twice = direction.turn().turn().back()
+        val pointSW = twice.apply(twice.apply(pointNW))
+        val sum = listOf(pointN, pointW, pointNW, pointSW).map { values.getOrDefault(it, 0) }.sum()
+        values[this] = sum
+        return sum
+    }
 }
 
 enum class Direction {
@@ -47,27 +75,37 @@ enum class Direction {
         override fun apply(current: Point): Point = current.up()
 
         override fun turn(): Direction = LEFT
+
+        override fun back(): Direction = DOWN
     },
 
     DOWN {
         override fun apply(current: Point): Point = current.down()
 
         override fun turn(): Direction = RIGHT
+
+        override fun back(): Direction = UP
     },
 
     LEFT {
         override fun apply(current: Point): Point = current.left()
 
         override fun turn(): Direction = DOWN
+
+        override fun back(): Direction = RIGHT
     },
 
     RIGHT {
         override fun apply(current: Point): Point = current.right()
 
         override fun turn(): Direction = UP
+
+        override fun back(): Direction = LEFT
     };
 
     abstract fun apply(current: Point): Point
 
     abstract fun turn(): Direction
+
+    abstract fun back(): Direction
 }
--- src/test/kotlin/be/edu/adventofcode/y2017/day03/Day03Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2017/day03/Day03Spec.kt
@@ -4,13 +4,13 @@ import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-import org.jetbrains.spek.api.dsl.xon
+import org.jetbrains.spek.api.dsl.xit
 import kotlin.test.assertEquals
 
 object Day03Spec : Spek({
     given("day 03") {
         val day03 = Day03()
-        xon("part 1") {
+        on("part 1") {
             it("Data from square 1 is carried 0 steps, since it's at the access port.") {
                 assertEquals(0, day03.part1(1))
             }
@@ -23,28 +23,28 @@ object Day03Spec : Spek({
             it("Data from square 1024 must be carried 31 steps.") {
                 assertEquals(31, day03.part1(1024))
             }
-            it("should answer How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?") {
+            xit("should answer How many steps are required to carry the data from the square identified in your puzzle input all the way to the access port?") {
                 assertEquals(430, day03.part1(312051))
             }
         }
         on("part 2") {
             it("Square 1 starts with the value 1.") {
-                assertEquals(1, day03.part2(1))
+                assertEquals(2, day03.part2(1))
             }
             it("Square 2 has only one adjacent filled square (with value 1), so it also stores 1.") {
-                assertEquals(1, day03.part2(2))
+                assertEquals(4, day03.part2(2))
             }
             it("Square 3 has both of the above squares as neighbors and stores the sum of their values, 2.") {
-                assertEquals(2, day03.part2(3))
+                assertEquals(4, day03.part2(3))
             }
             it("Square 4 has all three of the aforementioned squares as neighbors and stores the sum of their values, 4.") {
-                assertEquals(4, day03.part2(4))
+                assertEquals(5, day03.part2(4))
             }
             it("Square 5 only has the first and fourth squares as neighbors, so it gets the value 5.") {
-                assertEquals(5, day03.part2(5))
+                assertEquals(10, day03.part2(5))
             }
             it("should answer What is the first value written that is larger than your puzzle input?") {
-                assertEquals(0, day03.part2(312051))
+                assertEquals(312453, day03.part2(312051))
             }
         }
     }
