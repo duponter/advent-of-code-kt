21cdeae2c2852c2bcf8f5243e44e021dc2153256
Erwin Dupont <erwin.dupont@aca-it.be>
2017-12-08 13:21:16 +0100

    [2017 - Day 08] Solved part 1 and 2

--- src/main/kotlin/be/edu/adventofcode/y2017/day08/Day08.kt
+++ src/main/kotlin/be/edu/adventofcode/y2017/day08/Day08.kt
@@ -1,13 +1,76 @@
 package be.edu.adventofcode.y2017.day08
 
 import be.edu.adventofcode.Lines
+import kotlin.math.max
 
 class Day08 {
     fun part1(input: Lines): Int {
-        return input.get().count()
+        val regex = Regex("([a-z]+) (inc|dec) (-?\\d+) if ([a-z]+) ([!=<>]=?) (-?\\d+)")
+
+        val tokens = input.get().map { regex.matchEntire(it) }.map { it!!.groupValues }
+        val registers = tokens.flatMap { listOf(it[1], it[4]) }.toSet().groupBy({ it }, { Register(it) }).mapValues { it.value.single() }
+        tokens.map { toInstruction(it, registers) }.map(Instruction::execute)
+        return registers.maxBy { it.value.value }!!.value.value
+    }
+
+    private fun toInstruction(tokens: List<String>, registers: Map<String, Register>): Instruction {
+        val operation = Operation(registers[tokens[1]]!!, tokens[3].toInt(), Operator.valueOf(tokens[2].toUpperCase()))
+        val condition = Condition(registers[tokens[4]]!!, tokens[6].toInt(), ConditionalOperator(tokens[5]))
+        return Instruction(operation, condition)
     }
 
     fun part2(input: Lines): Int {
-        return input.get().count()
+        val regex = Regex("([a-z]+) (inc|dec) (-?\\d+) if ([a-z]+) ([!=<>]=?) (-?\\d+)")
+
+        val tokens = input.get().map { regex.matchEntire(it) }.map { it!!.groupValues }
+        val registers = tokens.flatMap { listOf(it[1], it[4]) }.toSet().groupBy({ it }, { Register(it) }).mapValues { it.value.single() }
+        val instructions = tokens.map { toInstruction(it, registers) }
+        return instructions.fold(0, { acc, instruction -> max(acc, instruction.execute().value) })
+    }
+}
+
+data class Register(val name: String, var value: Int = 0)
+
+data class Instruction(private val operation: Operation, private val condition: Condition) {
+    fun execute(): Register {
+        println("Executing $this")
+        if (condition.execute()) operation.execute()
+        println("Changed register ${operation.register}")
+        return operation.register
+    }
+}
+
+data class Operation(var register: Register, private val operand: Int, private val operator: Operator) {
+    fun execute() {
+        register.value = operator.apply(register.value, operand)
+    }
+}
+
+enum class Operator {
+    INC {
+        override fun apply(registerValue: Int, operand: Int): Int = registerValue + operand
+    },
+    DEC {
+        override fun apply(registerValue: Int, operand: Int): Int = registerValue - operand
+    };
+
+    abstract fun apply(registerValue: Int, operand: Int): Int
+}
+
+data class Condition(private var register: Register, private val operand: Int, private val operator: ConditionalOperator) {
+    fun execute(): Boolean = operator.apply(register.value, operand)
+}
+
+data class ConditionalOperator(val value: String) {
+    fun apply(registerValue: Int, operand: Int): Boolean {
+        return when (value) {
+            ">" -> registerValue > operand
+            ">=" -> registerValue >= operand
+            "<" -> registerValue < operand
+            "<=" -> registerValue <= operand
+            "==" -> registerValue == operand
+            "!=" -> registerValue != operand
+            else -> throw UnsupportedOperationException(value)
+        }
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2017/day08/Day08Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2017/day08/Day08Spec.kt
@@ -12,19 +12,25 @@ object Day08Spec : Spek({
     given("day 08") {
         val day08 = Day08()
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day08.part1(LinesFromArray("line")))
+            it("After this process, the largest value in any register is 1.") {
+                assertEquals(1, day08.part1(LinesFromArray("b inc 5 if a > 1",
+                        "a inc 1 if b < 5",
+                        "c dec -10 if a >= 1",
+                        "c inc -20 if c == 10")))
             }
-            it("should answer Part1") {
-                assertEquals(0, day08.part1(DayInput().lines(day08)))
+            it("should answer What is the largest value in any register?") {
+                assertEquals(5849, day08.part1(DayInput().lines(day08)))
             }
         }
         on("part 2") {
-            it("testcase") {
-                assertEquals(1, day08.part2(LinesFromArray("line")))
+            it("The highest value held in any register during this process is 10") {
+                assertEquals(10, day08.part2(LinesFromArray("b inc 5 if a > 1",
+                        "a inc 1 if b < 5",
+                        "c dec -10 if a >= 1",
+                        "c inc -20 if c == 10")))
             }
-            it("should answer Part2") {
-                assertEquals(0, day08.part2(DayInput().lines(day08)))
+            it("should answer The highest value held in any register during this process") {
+                assertEquals(6702, day08.part2(DayInput().lines(day08)))
             }
         }
     }
