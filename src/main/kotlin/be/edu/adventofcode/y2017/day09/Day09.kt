c07f7fa6a9416e6331f369921fc9c174e3a75f64
Erwin Dupont <erwin.dupont@aca-it.be>
2017-12-09 13:50:31 +0100

    [2017 - Day 09] Solved part 1 and 2

--- src/main/kotlin/be/edu/adventofcode/y2017/day09/Day09.kt
+++ src/main/kotlin/be/edu/adventofcode/y2017/day09/Day09.kt
@@ -1,13 +1,69 @@
 package be.edu.adventofcode.y2017.day09
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day09 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Text): Int {
+        return Regex("!.").replace(input.get(), "").asIterable()
+                .fold(Group(), { acc: Context, c: Char -> acc.apply(c) })
+                .groups()
     }
 
-    fun part2(input: Lines): Int {
-        return input.get().count()
+    fun part2(input: Text): Int {
+        return Regex("!.").replace(input.get(), "").asIterable()
+                .fold(Group(), { acc: Context, c: Char -> acc.apply2(c) })
+                .garbage()
     }
+}
+
+interface Context {
+    fun apply(next: Char): Context
+
+    fun apply2(next: Char): Context
+
+    fun groups(): Int
+
+    fun garbage(): Int
+}
+
+data class Group(private val totalValue: Int = 0, private val depth: Int = 0) : Context {
+    override fun apply(next: Char): Context {
+        return when (next) {
+            '{' -> Group(totalValue, depth + 1)
+            '}' -> Group(totalValue + depth, depth - 1)
+            '<' -> Garbage(this)
+            else -> this
+        }
+    }
+
+    override fun apply2(next: Char): Context {
+        return when (next) {
+            '<' -> Garbage(this, totalValue)
+            else -> this
+        }
+    }
+
+    override fun groups(): Int = totalValue
+
+    override fun garbage(): Int = totalValue
+}
+
+data class Garbage(private val group: Group, private val count: Int = 0) : Context {
+    override fun apply(next: Char): Context {
+        return when (next) {
+            '>' -> group
+            else -> this
+        }
+    }
+
+    override fun apply2(next: Char): Context {
+        return when (next) {
+            '>' -> Group(count)
+            else -> Garbage(group, count + 1)
+        }
+    }
+
+    override fun groups(): Int = group.groups()
+
+    override fun garbage(): Int = count
 }
--- src/test/kotlin/be/edu/adventofcode/y2017/day09/Day09Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2017/day09/Day09Spec.kt
@@ -1,7 +1,7 @@
 package be.edu.adventofcode.y2017.day09
 
 import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -12,19 +12,58 @@ object Day09Spec : Spek({
     given("day 09") {
         val day09 = Day09()
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day09.part1(LinesFromArray("line")))
+            it("{}, score of 1.") {
+                assertEquals(1, day09.part1(TextFromString("{}")))
             }
-            it("should answer Part1") {
-                assertEquals(0, day09.part1(DayInput().lines(day09)))
+            it("{{{}}}, score of 1 + 2 + 3 = 6.") {
+                assertEquals(6, day09.part1(TextFromString("{{{}}}")))
+            }
+            it("{{},{}}, score of 1 + 2 + 2 = 5.") {
+                assertEquals(5, day09.part1(TextFromString("{{},{}}")))
+            }
+            it("{{{},{},{{}}}}, score of 1 + 2 + 3 + 3 + 3 + 4 = 16.") {
+                assertEquals(16, day09.part1(TextFromString("{{{},{},{{}}}}")))
+            }
+            it("{<a>,<a>,<a>,<a>}, score of 1.") {
+                assertEquals(1, day09.part1(TextFromString("{<a>,<a>,<a>,<a>}")))
+            }
+            it("{{<ab>},{<ab>},{<ab>},{<ab>}}, score of 1 + 2 + 2 + 2 + 2 = 9.") {
+                assertEquals(9, day09.part1(TextFromString("{{<ab>},{<ab>},{<ab>},{<ab>}}")))
+            }
+            it("{{<!!>},{<!!>},{<!!>},{<!!>}}, score of 1 + 2 + 2 + 2 + 2 = 9.") {
+                assertEquals(9, day09.part1(TextFromString("{{<!!>},{<!!>},{<!!>},{<!!>}}")))
+            }
+            it("{{<a!>},{<a!>},{<a!>},{<ab>}}, score of 1 + 2 = 3") {
+                assertEquals(3, day09.part1(TextFromString("{{<a!>},{<a!>},{<a!>},{<ab>}}")))
+            }
+            it("should answer What is the total score for all groups in your input?") {
+                assertEquals(20530, day09.part1(DayInput().text(day09)))
             }
         }
         on("part 2") {
-            it("testcase") {
-                assertEquals(1, day09.part2(LinesFromArray("line")))
+            it("<>, 0 characters.") {
+                assertEquals(0, day09.part2(TextFromString("<>")))
             }
-            it("should answer Part2") {
-                assertEquals(0, day09.part2(DayInput().lines(day09)))
+            it("<random characters>, 17 characters.") {
+                assertEquals(17, day09.part2(TextFromString("<random characters>")))
+            }
+            it("<<<<>, 3 characters.") {
+                assertEquals(3, day09.part2(TextFromString("<<<<>")))
+            }
+            it("<{!>}>, 2 characters.") {
+                assertEquals(2, day09.part2(TextFromString("<{!>}>")))
+            }
+            it("<!!>, 0 characters.") {
+                assertEquals(0, day09.part2(TextFromString("<!!>")))
+            }
+            it("<!!!>>, 0 characters.") {
+                assertEquals(0, day09.part2(TextFromString("<!!!>>")))
+            }
+            it("<{o\"i!a,<{i<a>, 10 characters.") {
+                assertEquals(10, day09.part2(TextFromString("<{o\"i!a,<{i<a>")))
+            }
+            it("should answer How many non-canceled characters are within the garbage in your puzzle input?") {
+                assertEquals(9978, day09.part2(DayInput().text(day09)))
             }
         }
     }
