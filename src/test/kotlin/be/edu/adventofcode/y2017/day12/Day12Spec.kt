86f8488922676d9e9fbc02ca5c7fa07aff8f850c
Erwin Dupont <erwin.dupont@aca-it.be>
2017-12-11 12:33:43 +0100

    [2017 - Day 12] Generation

---
+++ src/test/kotlin/be/edu/adventofcode/y2017/day12/Day12Spec.kt
@@ -0,0 +1,31 @@
+package be.edu.adventofcode.y2017.day12
+
+import be.edu.adventofcode.DayInput
+import be.edu.adventofcode.LinesFromArray
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.given
+import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
+import kotlin.test.assertEquals
+
+object Day12Spec : Spek({
+    given("day 12") {
+        val day12 = Day12()
+        on("part 1") {
+            it("testcase") {
+                assertEquals(1, day12.part1(LinesFromArray("line")))
+            }
+            it("should answer Part1") {
+                assertEquals(0, day12.part1(DayInput().lines(day12)))
+            }
+        }
+        on("part 2") {
+            it("testcase") {
+                assertEquals(1, day12.part2(LinesFromArray("line")))
+            }
+            it("should answer Part2") {
+                assertEquals(0, day12.part2(DayInput().lines(day12)))
+            }
+        }
+    }
+})
