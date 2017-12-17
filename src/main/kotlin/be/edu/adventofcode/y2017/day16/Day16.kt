ac2ebd5cc458269821efb20815d22590c398c039
Erwin Dupont <erwin.dupont@aca-it.be>
2017-12-17 15:01:52 +0100

    [2017 - Day 16] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2017/day16/Day16.kt
+++ src/main/kotlin/be/edu/adventofcode/y2017/day16/Day16.kt
@@ -1,13 +1,59 @@
 package be.edu.adventofcode.y2017.day16
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day16 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(programs: String, input: Text): String {
+        return input.get().split(",")
+                .map { DanceMove.parse(it) }
+                .fold(programs, { acc, danceMove -> danceMove.perform(acc) })
     }
 
-    fun part2(input: Lines): Int {
+    fun part2(input: Text): Int {
         return input.get().count()
+    }
+}
+
+interface DanceMove {
+    fun perform(programs: String): String
+
+    companion object {
+        fun parse(input: String): DanceMove {
+            return Spin.parse(input) ?: Exchange.parse(input) ?: Partner.parse(input)!!
+        }
+    }
+}
+
+data class Spin(val count: Int) : DanceMove {
+    override fun perform(programs: String): String = programs.takeLast(count).plus(programs.dropLast(count))
+
+    companion object {
+        fun parse(input: String): Spin? {
+            val matchResult = Regex("s(\\d+)").matchEntire(input)
+            return if (matchResult != null) Spin(matchResult.destructured.component1().toInt()) else null
+        }
+    }
+}
+
+data class Exchange(val first: Int, val second: Int) : DanceMove {
+    override fun perform(programs: String): String = Partner(programs[first], programs[second]).perform(programs)
+
+    companion object {
+        fun parse(input: String): Exchange? {
+            val matchResult = Regex("x(\\d+)/(\\d+)").matchEntire(input)
+            return if (matchResult != null) Exchange(matchResult.destructured.component1().toInt(), matchResult.destructured.component2().toInt()) else null
+        }
+    }
+
+}
+
+data class Partner(val first: Char, val second: Char) : DanceMove {
+    override fun perform(programs: String): String = programs.replace(first, 'x').replace(second, first).replace('x', second)
+
+    companion object {
+        fun parse(input: String): Partner? {
+            val matchResult = Regex("p(\\w)/(\\w)").matchEntire(input)
+            return if (matchResult != null) Partner(matchResult.destructured.component1().single(), matchResult.destructured.component2().single()) else null
+        }
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2017/day16/Day16Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2017/day16/Day16Spec.kt
@@ -1,7 +1,7 @@
 package be.edu.adventofcode.y2017.day16
 
 import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -12,19 +12,16 @@ object Day16Spec : Spek({
     given("day 16") {
         val day16 = Day16()
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day16.part1(LinesFromArray("line")))
+            it("After finishing their dance, the programs end up in order baedc.") {
+                assertEquals("baedc", day16.part1("abcde", TextFromString("s1,x3/4,pe/b")))
             }
-            it("should answer Part1") {
-                assertEquals(0, day16.part1(DayInput().lines(day16)))
+            it("should answer In what order are the programs standing after their dance?") {
+                assertEquals("glnacbhedpfjkiom", day16.part1(CharRange('a', 'p').joinToString(""), DayInput().text(day16)))
             }
         }
         on("part 2") {
-            it("testcase") {
-                assertEquals(1, day16.part2(LinesFromArray("line")))
-            }
-            it("should answer Part2") {
-                assertEquals(0, day16.part2(DayInput().lines(day16)))
+            it("should answer In what order are the programs standing after their billion dances?") {
+                assertEquals(0, day16.part2(DayInput().text(day16)))
             }
         }
     }
