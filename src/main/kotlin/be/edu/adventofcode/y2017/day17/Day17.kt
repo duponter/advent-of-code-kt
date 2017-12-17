7e43e0d2fb81622b375f9fbc2d54ec5e92e90504
Erwin Dupont <erwin.dupont@aca-it.be>
2017-12-17 23:40:43 +0100

    [2017 - Day 17] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2017/day17/Day17.kt
+++ src/main/kotlin/be/edu/adventofcode/y2017/day17/Day17.kt
@@ -1,13 +1,30 @@
 package be.edu.adventofcode.y2017.day17
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.y2017.day10.split
 
 class Day17 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Int): Int {
+        val values = spinlock(listOf(0), 0, input)
+        return values[values.indexOf(2017) + 1]
     }
 
-    fun part2(input: Lines): Int {
-        return input.get().count()
+    private tailrec fun spinlock(values: List<Int>, current: Int, forward: Int): List<Int> {
+        val next = values.forward(current, forward)
+        val afterInsert = values.insert(values.size, next)
+        return if (afterInsert.size == 2018) afterInsert else spinlock(afterInsert, next, forward)
     }
+
+    fun part2(input: Int): Int {
+        return input
+    }
+}
+
+private fun <T> List<T>.forward(current: Int, forward: Int): Int {
+    val next = current + forward
+    return 1 + if (next < this.size) next else (next % this.size)
+}
+
+private fun <T> List<T>.insert(element: T, index: Int): List<T> {
+    val split = this.split(index)
+    return split.first.plus(element).plus(split.second)
 }
--- src/test/kotlin/be/edu/adventofcode/y2017/day17/Day17Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2017/day17/Day17Spec.kt
@@ -1,7 +1,5 @@
 package be.edu.adventofcode.y2017.day17
 
-import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.given
 import org.jetbrains.spek.api.dsl.it
@@ -12,19 +10,19 @@ object Day17Spec : Spek({
     given("day 17") {
         val day17 = Day17()
         on("part 1") {
-            it("testcase") {
-                assertEquals(1, day17.part1(LinesFromArray("line")))
+            it("To short-circuit the spinlock, identify the value that will ultimately be after the last value written (2017)") {
+                assertEquals(638, day17.part1(3))
             }
-            it("should answer Part1") {
-                assertEquals(0, day17.part1(DayInput().lines(day17)))
+            it("should answer What is the value after 2017 in your completed circular buffer?") {
+                assertEquals(808, day17.part1(356))
             }
         }
         on("part 2") {
             it("testcase") {
-                assertEquals(1, day17.part2(LinesFromArray("line")))
+                assertEquals(1, day17.part2(3))
             }
             it("should answer Part2") {
-                assertEquals(0, day17.part2(DayInput().lines(day17)))
+                assertEquals(0, day17.part2(356))
             }
         }
     }
