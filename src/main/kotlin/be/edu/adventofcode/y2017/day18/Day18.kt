9645e4bd67c7adde270aa3bf6e8e8ef0d14b4036
Erwin Dupont <erwin.dupont@aca-it.be>
2017-12-19 22:37:32 +0100

    [2017 - Day 18] Start implementation part 1

--- src/main/kotlin/be/edu/adventofcode/y2017/day18/Day18.kt
+++ src/main/kotlin/be/edu/adventofcode/y2017/day18/Day18.kt
@@ -4,6 +4,7 @@ import be.edu.adventofcode.Lines
 
 class Day18 {
     fun part1(input: Lines): Int {
+        input.get().map { Instruction.parse(it) }.forEach { println(it) }
         return input.get().count()
     }
 
@@ -14,38 +15,97 @@ class Day18 {
 
 sealed class Instruction {
     companion object {
-        fun parse(input: String) {
-
+        fun parse(input: String): Instruction {
+            val tokens = input.split(" ")
+            return when {
+                tokens.first() == "snd" -> Sound(tokens.last().first())
+                tokens.first() == "set" -> Assign(tokens[1].first(), parseRegister(tokens.last()))
+                tokens.first() == "add" -> Increase(tokens[1].first(), parseRegister(tokens.last()))
+                tokens.first() == "mul" -> Multiplication(tokens[1].first(), parseRegister(tokens.last()))
+                tokens.first() == "mod" -> Modulo(tokens[1].first(), parseRegister(tokens.last()))
+                tokens.first() == "rcv" -> Recover(tokens.last().first())
+                tokens.first() == "jgz" -> Jump(tokens[1].first(), parseRegister(tokens.last()))
+                else -> throw UnsupportedOperationException(input)
+            }
         }
+
+        private fun parseRegister(input: String): Instruction = if (input.matches("-?\\d+".toRegex())) Constant(input.toInt()) else Lookup(input.first())
+    }
+
+    abstract fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int
+}
+
+data class Constant(val value: Int) : Instruction() {
+    override fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int = value
+}
+
+data class Lookup(private val register: Char) : Instruction() {
+    override fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int = registers[register] ?: 0
+}
+
+data class Sound(private val register: Char) : Instruction() {
+    override fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int {
+        //snd X plays a sound with a frequency equal to the value of X.
+        return Lookup(register).apply(registers, instructions)
     }
 }
 
-data class Sound(val frequency: Int) : Instruction() {
-//snd X plays a sound with a frequency equal to the value of X.
+data class Assign(private val register: Char, private val instruction: Instruction) : Instruction() {
+    override fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int {
+        //set X Y sets register X to the value of Y.
+        val value = instruction.apply(registers, instructions)
+        registers[register] = value
+        return value
+    }
 }
 
-data class Set(val register: Char, val value: Int) : Instruction() {
-//set X Y sets register X to the value of Y.
+data class Increase(private val register: Char, private val instruction: Instruction) : Instruction() {
+    override fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int {
+        //add X Y increases register X by the value of Y.
+        val value = Lookup(register).apply(registers, instructions) + instruction.apply(registers, instructions)
+        registers[register] = value
+        return value
+    }
 }
 
-data class Increase(val register: Char, val value: Int) : Instruction() {
-//add X Y increases register X by the value of Y.
+data class Multiplication(private val register: Char, private val instruction: Instruction) : Instruction() {
+    override fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int {
+        //mul X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
+        val value = Lookup(register).apply(registers, instructions) * instruction.apply(registers, instructions)
+        registers[register] = value
+        return value
+    }
 }
 
-data class Multiplication(val register: Char, val value: Int) : Instruction() {
-//mul X Y sets register X to the result of multiplying the value contained in register X by the value of Y.
+data class Modulo(private val register: Char, private val instruction: Instruction) : Instruction() {
+    override fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int {
+        //mod X Y sets register X to the remainder of dividing the value contained in register X by the value of Y (that is, it sets X to the result of X modulo Y).
+        val value = Lookup(register).apply(registers, instructions) % instruction.apply(registers, instructions)
+        registers[register] = value
+        return value
+    }
 }
 
-data class Modulo(val register: Char, val value: Int) : Instruction() {
-//mod X Y sets register X to the remainder of dividing the value contained in register X by the value of Y (that is, it sets X to the result of X modulo Y).
+data class Recover(private val register: Char) : Instruction() {
+    override fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int {
+        //rcv X recovers the frequency of the last sound played, but only when the value of X is not zero. (If it is zero, the command does nothing.)
+        return if (Lookup(register).apply(registers, instructions) != 0) {
+//            instructions.takeWhile { it != this }.filter { it. }
+//                    .filterIsInstance(Sound.class)
+            0
+        } else 0
+
+    }
 }
 
-data class Recover(val register: Char) : Instruction() {
-//rcv X recovers the frequency of the last sound played, but only when the value of X is not zero. (If it is zero, the command does nothing.)
-}
+data class Jump(private val register: Char, private val instruction: Instruction) : Instruction() {
+    override fun apply(registers: MutableMap<Char, Int>, instructions: List<Instruction>): Int {
+        //jgz X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero. (An offset of 2 skips the next instruction, an offset of -1 jumps to the previous instruction, and so on.)
+        return if (Lookup(register).apply(registers, instructions) > 0) {
+            instructions[instructions.indexOf(this) + instruction.apply(registers, instructions)].apply(registers, instructions)
+        } else 0
 
-data class Jump(val register: Char, val offset: Int) : Instruction() {
-//jgz X Y jumps with an offset of the value of Y, but only if the value of X is greater than zero. (An offset of 2 skips the next instruction, an offset of -1 jumps to the previous instruction, and so on.)
+    }
 }
 
 
