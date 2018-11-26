bbf6ebd392f52765d86879051f085b8a899e0323
Erwin Dupont <erwin.dupont@aca-it.be>
2018-11-26 20:35:53 +0100

    [2018 - Day 01] Generation

---
+++ src/main/kotlin/be/edu/adventofcode/y2018/day01/Day01.kt
@@ -0,0 +1,13 @@
+package be.edu.adventofcode.y2018.day01
+
+import be.edu.adventofcode.Lines
+
+class Day01 {
+    fun part1(input: Lines): Int {
+        return input.get().count()
+    }
+
+	fun part2(input: Lines): Int {
+        return input.get().count()
+    }
+}
