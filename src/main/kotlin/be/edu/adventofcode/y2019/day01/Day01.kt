9eef7341dc17f2a503a298313add9b4394fa9aa5
Erwin Dupont <erwin.dupont@aca-it.be>
2019-12-01 23:18:02 +0100

    [2019 - Day 01] Solved part 1 & 2

--- src/main/kotlin/be/edu/adventofcode/y2019/day01/Day01.kt
+++ src/main/kotlin/be/edu/adventofcode/y2019/day01/Day01.kt
@@ -4,10 +4,27 @@ import be.edu.adventofcode.Lines
 
 class Day01 {
     fun part1(input: Lines): Int {
-        return input.get().count()
+        return calculateFuel(input.get()
+                .map { it.toInt() })
+    }
+
+    private fun calculateFuel(modules: List<Int>): Int {
+        return modules.map { it.div(3) }
+                .map { it - 2 }
+                .sum()
     }
 
 	fun part2(input: Lines): Int {
-        return input.get().count()
+        return input.get()
+                .map { it.toInt() }
+                .map { calculateFuel(listOf(it)) }
+                .map { includeAddedFuel(it) }
+                .sum()
+    }
+
+	private fun includeAddedFuel(input: Int): Int {
+        return generateSequence(input, { calculateFuel(listOf(it)) })
+                .takeWhile { it > 0 }
+                .sum()
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2019/day01/Day01Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2019/day01/Day01Spec.kt
@@ -9,20 +9,25 @@ import kotlin.test.assertEquals
 object Day01Spec : Spek({
     describe("day 01") {
         val day01 = Day01()
-        context("part 1") {
+        xcontext("part 1") {
             it("testcase") {
-                assertEquals(1, day01.part1(LinesFromArray("line")))
+                assertEquals(2, day01.part1(LinesFromArray("12")))
+                assertEquals(2, day01.part1(LinesFromArray("14")))
+                assertEquals(654, day01.part1(LinesFromArray("1969")))
+                assertEquals(33583, day01.part1(LinesFromArray("100756")))
             }
             it("should answer Part1") {
-                assertEquals(0, day01.part1(DayInput().lines(day01)))
+                assertEquals(3481005, day01.part1(DayInput().lines(day01)))
             }
         }
         context("part 2") {
             it("testcase") {
-                assertEquals(1, day01.part2(LinesFromArray("line")))
+                assertEquals(2, day01.part2(LinesFromArray("14")))
+                assertEquals(966, day01.part2(LinesFromArray("1969")))
+                assertEquals(50346, day01.part2(LinesFromArray("100756")))
             }
             it("should answer Part2") {
-                assertEquals(0, day01.part2(DayInput().lines(day01)))
+                assertEquals(5218616, day01.part2(DayInput().lines(day01)))
             }
         }
     }
