f10a01d0696490f50289973ee6eeed425dd207ba
Erwin Dupont <erwin.dupont@aca-it.be>
2019-12-02 08:17:31 +0100

    [2019 - Day 02] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2019/day02/Day02.kt
+++ src/main/kotlin/be/edu/adventofcode/y2019/day02/Day02.kt
@@ -1,13 +1,40 @@
 package be.edu.adventofcode.y2019.day02
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day02 {
-    fun part1(input: Lines): Int {
+    fun part1(input: Text): Int {
+        val mutableList = input.get().split(',')
+                .map { it.toInt() }
+                .toMutableList()
+        mutableList[1] = 12
+        mutableList[2] = 2
+
+        return performOperations(mutableList)[0]
+    }
+
+    fun performOperations(mutableList: MutableList<Int>): MutableList<Int> {
+        for (i in mutableList.indices step 4) {
+            if (mutableList[i] == 99) break
+            Operation(mutableList[i], mutableList[i + 1], mutableList[i + 2], mutableList[i + 3]).apply(mutableList)
+        }
+        return mutableList
+    }
+
+    fun part2(input: Text): Int {
         return input.get().count()
     }
 
-    fun part2(input: Lines): Int {
-        return input.get().count()
+    class Operation(private val opCode: Int, private val posInput1: Int, private val posInput2: Int, private val posResult: Int) {
+        fun apply(values: MutableList<Int>) {
+            val input1 = values[posInput1]
+            val input2 = values[posInput2]
+            if (opCode == 1) {
+                values[posResult] = input1 + input2
+            } else if (opCode == 2) {
+                values[posResult] = input1 * input2
+            }
+        }
+
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2019/day02/Day02Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2019/day02/Day02Spec.kt
@@ -1,7 +1,7 @@
 package be.edu.adventofcode.y2019.day02
 
 import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
@@ -11,18 +11,21 @@ object Day02Spec : Spek({
         val day02 = Day02()
         context("part 1") {
             it("testcase") {
-                assertEquals(1, day02.part1(LinesFromArray("line")))
+                assertEquals(mutableListOf(2,0,0,0,99), day02.performOperations(mutableListOf(1,0,0,0,99)))
+                assertEquals(mutableListOf(2,3,0,6,99), day02.performOperations(mutableListOf(2,3,0,3,99)))
+                assertEquals(mutableListOf(2,4,4,5,99,9801), day02.performOperations(mutableListOf(2,4,4,5,99,0)))
+                assertEquals(mutableListOf(30,1,1,4,2,5,6,0,99), day02.performOperations(mutableListOf(1,1,1,4,99,5,6,0,99)))
             }
             it("should answer Part1") {
-                assertEquals(0, day02.part1(DayInput().lines(day02)))
+                assertEquals(4930687, day02.part1(DayInput().text(day02)))
             }
         }
-        context("part 2") {
+        xcontext("part 2") {
             it("testcase") {
-                assertEquals(1, day02.part2(LinesFromArray("line")))
+                assertEquals(1, day02.part2(TextFromString("line")))
             }
             it("should answer Part2") {
-                assertEquals(0, day02.part2(DayInput().lines(day02)))
+                assertEquals(0, day02.part2(DayInput().text(day02)))
             }
         }
     }
