00c5413976cad65b7cd92c7bd7108616679a7b91
Erwin Dupont <erwin.dupont@aca-it.be>
2019-12-03 21:03:32 +0100

    [2019 - Day 03] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2019/day03/Day03.kt
+++ src/main/kotlin/be/edu/adventofcode/y2019/day03/Day03.kt
@@ -1,13 +1,61 @@
 package be.edu.adventofcode.y2019.day03
 
 import be.edu.adventofcode.Lines
+import kotlin.math.abs
 
 class Day03 {
     fun part1(input: Lines): Int {
-        return input.get().count()
+        return wirePath(input.get().first())
+                .intersect(wirePath(input.get().last()))
+                .map { it.distance() }
+                .min() ?: 0
+    }
+
+    private fun wirePath(wire: String): List<Point> {
+        return wire.split(',')
+                .map { Pair(Direction.valueOf(it.first().toString()), it.drop(1).toInt()) }
+                .fold(listOf(Point()), { path: List<Point>, pair: Pair<Direction, Int> -> path.plus(pair.first.path(path.last(), pair.second)) })
+                .drop(1)
     }
 
     fun part2(input: Lines): Int {
         return input.get().count()
+    }
+}
+
+data class Point(val x: Int = 0, val y: Int = 0) {
+    fun distance(): Int = distance(Point())
+
+    fun distance(other: Point): Int = abs(x - other.x) + abs(y - other.y)
+
+    fun up(): Point = Point(x, y + 1)
+
+    fun down(): Point = Point(x, y - 1)
+
+    fun left(): Point = Point(x - 1, y)
+
+    fun right(): Point = Point(x + 1, y)
+}
+
+enum class Direction {
+    U {
+        override fun next(start: Point): Point = start.up()
+    },
+    D {
+        override fun next(start: Point): Point = start.down()
+    },
+    L {
+        override fun next(start: Point): Point = start.left()
+    },
+    R {
+        override fun next(start: Point): Point = start.right()
+    };
+
+    abstract fun next(start: Point): Point
+
+    fun path(start: Point, steps: Int): Sequence<Point> {
+        return generateSequence(start) { this.next(it) }
+                .drop(1)
+                .take(steps)
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2019/day03/Day03Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2019/day03/Day03Spec.kt
@@ -11,13 +11,14 @@ object Day03Spec : Spek({
         val day03 = Day03()
         context("part 1") {
             it("testcase") {
-                assertEquals(1, day03.part1(LinesFromArray("line")))
+                assertEquals(159, day03.part1(LinesFromArray("R75,D30,R83,U83,L12,D49,R71,U7,L72", "U62,R66,U55,R34,D71,R55,D58,R83")))
+                assertEquals(135, day03.part1(LinesFromArray("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51", "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")))
             }
-            it("should answer Part1") {
-                assertEquals(0, day03.part1(DayInput().lines(day03)))
+            it("What is the Manhattan distance from the central port to the closest intersection?") {
+                assertEquals(280, day03.part1(DayInput().lines(day03)))
             }
         }
-        context("part 2") {
+        xcontext("part 2") {
             it("testcase") {
                 assertEquals(1, day03.part2(LinesFromArray("line")))
             }
