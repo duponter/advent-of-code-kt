a9c61331c7d303ea96ceaace2aa842a914c9ab7e
Erwin Dupont <erwin.dupont@aca-it.be>
2019-12-04 11:36:28 +0100

    [2019 - Day 04] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2019/day04/Day04.kt
+++ src/main/kotlin/be/edu/adventofcode/y2019/day04/Day04.kt
@@ -1,13 +1,36 @@
 package be.edu.adventofcode.y2019.day04
 
 import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
+import kotlin.math.abs
+import kotlin.math.log10
 
 class Day04 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Text): Int {
+        val range = input.get().split("-").map { it.toInt() }
+        return IntRange(range.first(), range.last())
+                .filter { valid(it.toString()) }
+                .count()
+    }
+
+    fun valid(password: String): Boolean = valid(password.toList().map { it.toInt() }.toIntArray())
+
+    private fun valid(password: IntArray): Boolean {
+        val sixDigits = { ints: IntArray -> ints.size == 6 }
+        val neverDecrease = { ints: IntArray -> ints.sortedArray().contentEquals(ints) }
+        val sameAdjacentDigits = { ints: IntArray -> ints.toList()
+                    .windowed(2) { it.distinct().size }
+                    .any { it == 1 }
+        }
+        return sixDigits(password) && neverDecrease(password) && sameAdjacentDigits(password)
     }
 
     fun part2(input: Lines): Int {
         return input.get().count()
     }
+}
+
+fun Int.length() = when(this) {
+    0 -> 1
+    else -> log10(abs(toDouble())).toInt() + 1
 }
--- src/test/kotlin/be/edu/adventofcode/y2019/day04/Day04Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2019/day04/Day04Spec.kt
@@ -2,22 +2,27 @@ package be.edu.adventofcode.y2019.day04
 
 import be.edu.adventofcode.DayInput
 import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 object Day04Spec : Spek({
     describe("day 04") {
         val day04 = Day04()
         context("part 1") {
             it("testcase") {
-                assertEquals(1, day04.part1(LinesFromArray("line")))
+                assertTrue(day04.valid("111111"))
+                assertFalse(day04.valid("223450"))
+                assertFalse(day04.valid("123789"))
             }
             it("should answer Part1") {
-                assertEquals(0, day04.part1(DayInput().lines(day04)))
+                assertEquals(1716, day04.part1(TextFromString("165432-707912")))
             }
         }
-        context("part 2") {
+        xcontext("part 2") {
             it("testcase") {
                 assertEquals(1, day04.part2(LinesFromArray("line")))
             }
