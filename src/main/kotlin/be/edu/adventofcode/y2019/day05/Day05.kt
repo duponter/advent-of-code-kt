05cc5e651888d9e1597dbee62b1d14b3b590c700
Erwin Dupont <erwin.dupont@aca-it.be>
2019-12-05 09:38:11 +0100

    [2019 - Day 05] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2019/day05/Day05.kt
+++ src/main/kotlin/be/edu/adventofcode/y2019/day05/Day05.kt
@@ -1,13 +1,73 @@
 package be.edu.adventofcode.y2019.day05
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day05 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Text, inputInstruction: Int): Int {
+        val mutableList = input.get().split(',')
+                .map { it.toInt() }
+                .toMutableList()
+
+        println("$mutableList")
+        var pair = Pair(0, -1)
+        while (pair.first != -1) {
+            pair = Operation(pair).apply(mutableList, inputInstruction)
+        }
+        return pair.second
     }
 
-    fun part2(input: Lines): Int {
+    fun part2(input: Text): Int {
         return input.get().count()
+    }
+}
+
+class Operation(private val pair: Pair<Int, Int>) {
+    fun apply(values: MutableList<Int>, input: Int): Pair<Int, Int> {
+        val index = pair.first
+        val opCode = values[index].toString().padStart(5, '0')
+        println("operation code $opCode")
+        val mode1 = ParameterMode.fromValue(opCode.drop(2).take(1).toInt())
+        val mode2 = ParameterMode.fromValue(opCode.drop(1).take(1).toInt())
+        val mode3 = ParameterMode.fromValue(opCode.take(1).toInt())
+        when (opCode.takeLast(2).toInt()) {
+            1 -> {
+                values[mode3.resolve(values, index + 3)] = mode1.resolve(values, values[index + 1]) + mode2.resolve(values, values[index + 2])
+                return Pair(index + 4, pair.second)
+            }
+            2 -> {
+                values[mode3.resolve(values, index + 3)] = mode1.resolve(values, values[index + 1]) * mode2.resolve(values, values[index + 2])
+                return Pair(index + 4, pair.second)
+            }
+            3 -> {
+                values[mode1.resolve(values, index + 1)] = input
+                return Pair(index + 2, pair.second)
+            }
+            4 -> {
+                val output = mode1.resolve(values, values[index + 1])
+                println("Output $output")
+                return Pair(index + 2, output)
+            }
+            99 -> return Pair(-1, pair.second)
+            else -> throw IllegalArgumentException("Invalid OpCode $opCode")
+        }
+    }
+}
+
+enum class ParameterMode {
+    POSITION {
+        override fun resolve(values: MutableList<Int>, parameter: Int): Int = values[parameter]
+    },
+    IMMEDIATE {
+        override fun resolve(values: MutableList<Int>, parameter: Int): Int = parameter
+    };
+
+    abstract fun resolve(values: MutableList<Int>, parameter: Int): Int
+
+    companion object {
+        fun fromValue(value: Int): ParameterMode = when (value) {
+            0 -> POSITION
+            1 -> IMMEDIATE
+            else -> throw IllegalArgumentException("Invalid ParameterMode $value")
+        }
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2019/day05/Day05Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2019/day05/Day05Spec.kt
@@ -1,7 +1,7 @@
 package be.edu.adventofcode.y2019.day05
 
 import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
@@ -11,18 +11,18 @@ object Day05Spec : Spek({
         val day05 = Day05()
         context("part 1") {
             it("testcase") {
-                assertEquals(1, day05.part1(LinesFromArray("line")))
+                assertEquals(88, day05.part1(TextFromString("3,0,4,0,99"), 88))
             }
             it("should answer Part1") {
-                assertEquals(0, day05.part1(DayInput().lines(day05)))
+                assertEquals(15314507, day05.part1(DayInput().text(day05), 1))
             }
         }
         context("part 2") {
             it("testcase") {
-                assertEquals(1, day05.part2(LinesFromArray("line")))
+                assertEquals(1, day05.part2(TextFromString("line")))
             }
             it("should answer Part2") {
-                assertEquals(0, day05.part2(DayInput().lines(day05)))
+                assertEquals(0, day05.part2(DayInput().text(day05)))
             }
         }
     }
