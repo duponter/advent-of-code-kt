886bebead50306381a411188dec7985cefacc298
Erwin Dupont <erwin.dupont@aca-it.be>
2019-12-06 10:35:10 +0100

    [2019 - Day 06] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2019/day06/Day06.kt
+++ src/main/kotlin/be/edu/adventofcode/y2019/day06/Day06.kt
@@ -4,10 +4,30 @@ import be.edu.adventofcode.Lines
 
 class Day06 {
     fun part1(input: Lines): Int {
-        return input.get().count()
+        val orbitMap = input.get()
+                .map { it.split(')') }
+                .groupBy( { it.first() }, { it.last() } )
+
+        return listOrbits(orbitMap, SpaceObject("COM")).values
+                .map { it.orbitCount() }
+                .sum()
+    }
+
+    private fun listOrbits(orbitMap: Map<String, List<String>>, spaceObject: SpaceObject): Map<String, SpaceObject> {
+        val tmp = mutableMapOf<String, SpaceObject>()
+        tmp[spaceObject.name] = spaceObject
+        orbitMap.getOrElse(spaceObject.name, { listOf() })
+                .map { listOrbits(orbitMap, SpaceObject(it, spaceObject)) }
+                .forEach { tmp.putAll(it) }
+        return tmp
     }
 
     fun part2(input: Lines): Int {
         return input.get().count()
     }
 }
+
+class SpaceObject(val name: String, private val orbitAround: SpaceObject? = null) {
+    fun orbitCount(): Int = if (orbitAround == null) 0 else orbitAround.orbitCount() + 1
+}
+
--- src/test/kotlin/be/edu/adventofcode/y2019/day06/Day06Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2019/day06/Day06Spec.kt
@@ -5,19 +5,21 @@ import be.edu.adventofcode.LinesFromArray
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
 
 object Day06Spec : Spek({
     describe("day 06") {
         val day06 = Day06()
         context("part 1") {
             it("testcase") {
-                assertEquals(1, day06.part1(LinesFromArray("line")))
+                assertEquals(42, day06.part1(LinesFromArray("COM)B", "B)C", "C)D", "D)E", "E)F", "B)G", "G)H", "D)I", "E)J", "J)K", "K)L")))
             }
-            it("should answer Part1") {
-                assertEquals(0, day06.part1(DayInput().lines(day06)))
+            it("What is the total number of direct and indirect orbits in your map data?") {
+                assertNotEquals(1709, day06.part1(DayInput().lines(day06)))
+                assertEquals(106065, day06.part1(DayInput().lines(day06)))
             }
         }
-        context("part 2") {
+        xcontext("part 2") {
             it("testcase") {
                 assertEquals(1, day06.part2(LinesFromArray("line")))
             }
