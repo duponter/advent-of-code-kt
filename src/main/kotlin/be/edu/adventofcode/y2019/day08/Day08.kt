2e47918e8e9427ffde37f9f00b844a926dbb9f47
Erwin Dupont <erwin.dupont@aca-it.be>
2019-12-08 17:28:43 +0100

    [2019 - Day 08] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2019/day08/Day08.kt
+++ src/main/kotlin/be/edu/adventofcode/y2019/day08/Day08.kt
@@ -1,13 +1,27 @@
 package be.edu.adventofcode.y2019.day08
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day08 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Text): Int {
+        val image = Image(25, 6)
+        val layer = image.asLayers(input.get().map { it.toString().toInt() })
+                .minBy { it.countDigit(0) }!!
+        return layer.countDigit(1) * layer.countDigit(2)
     }
 
-    fun part2(input: Lines): Int {
+    fun part2(input: Text): Int {
         return input.get().count()
     }
+}
+
+class Image(private val width: Int, private val height: Int) {
+    fun asLayers(digits: List<Int>): List<Layer> {
+        return digits.chunked(width * height) { it.chunked(width) }
+                .map { Layer(it) }
+    }
+}
+
+data class Layer(val pixels: List<List<Int>>) {
+    fun countDigit(digit: Int): Int = pixels.flatten().count { it == digit }
 }
--- src/test/kotlin/be/edu/adventofcode/y2019/day08/Day08Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2019/day08/Day08Spec.kt
@@ -1,7 +1,7 @@
 package be.edu.adventofcode.y2019.day08
 
 import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
@@ -10,19 +10,16 @@ object Day08Spec : Spek({
     describe("day 08") {
         val day08 = Day08()
         context("part 1") {
-            it("testcase") {
-                assertEquals(1, day08.part1(LinesFromArray("line")))
-            }
-            it("should answer Part1") {
-                assertEquals(0, day08.part1(DayInput().lines(day08)))
+            it("On the layer with the fewest 0 digits, what is the number of 1 digits multiplied by the number of 2 digits?") {
+                assertEquals(2032, day08.part1(DayInput().text(day08)))
             }
         }
-        context("part 2") {
+        xcontext("part 2") {
             it("testcase") {
-                assertEquals(1, day08.part2(LinesFromArray("line")))
+                assertEquals(1, day08.part2(TextFromString("line")))
             }
             it("should answer Part2") {
-                assertEquals(0, day08.part2(DayInput().lines(day08)))
+                assertEquals(0, day08.part2(DayInput().text(day08)))
             }
         }
     }
