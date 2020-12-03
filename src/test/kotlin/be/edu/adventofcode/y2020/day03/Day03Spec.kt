71d6787e6babf59e0b2495acce86c6a217391894
Erwin Dupont <erwin.dupont@aca-it.be>
2020-12-03 08:56:59 +0100

    [2020 - Day 03] Solved part 2

--- src/test/kotlin/be/edu/adventofcode/y2020/day03/Day03Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2020/day03/Day03Spec.kt
@@ -7,28 +7,26 @@ import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
 import kotlin.test.assertNotEquals
 
+val testInput = LinesFromArray(
+    "..##.........##.........##.........##.........##.........##.......",
+    "#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..",
+    ".#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.",
+    "..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#",
+    ".#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.",
+    "..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....",
+    ".#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#",
+    ".#........#.#........#.#........#.#........#.#........#.#........#",
+    "#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...",
+    "#...##....##...##....##...##....##...##....##...##....##...##....#",
+    ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"
+)
+
 object Day03Spec : Spek({
     describe("day 03") {
         val day03 = Day03()
         context("part 1") {
             it("testcase") {
-                assertEquals(
-                    7, day03.part1(
-                        LinesFromArray(
-                            "..##.........##.........##.........##.........##.........##.......",
-                            "#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..",
-                            ".#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.",
-                            "..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#",
-                            ".#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.",
-                            "..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....",
-                            ".#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#",
-                            ".#........#.#........#.#........#.#........#.#........#.#........#",
-                            "#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...",
-                            "#...##....##...##....##...##....##...##....##...##....##...##....#",
-                            ".#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#"
-                        )
-                    )
-                )
+                assertEquals(7, day03.part1(testInput))
             }
             it("Following a slope of right 3 and down 1, how many trees would you encounter?") {
                 assertNotEquals(94, day03.part1(DayInput().lines(day03)))
@@ -36,12 +34,17 @@ object Day03Spec : Spek({
                 assertEquals(272, day03.part1(DayInput().lines(day03)))
             }
         }
-        xcontext("part 2") {
+        context("part 2") {
             it("testcase") {
-                assertEquals(1, day03.part2(LinesFromArray("line")))
+                assertEquals(2, day03.slopeTreeCount(testInput,1,1))
+                assertEquals(7, day03.slopeTreeCount(testInput,3,1))
+                assertEquals(3, day03.slopeTreeCount(testInput,5,1))
+                assertEquals(4, day03.slopeTreeCount(testInput,7,1))
+                assertEquals(2, day03.slopeTreeCount(testInput,1,2))
+                assertEquals(336, day03.part2(testInput))
             }
-            it("should answer Part2") {
-                assertEquals(0, day03.part2(DayInput().lines(day03)))
+            it("What do you get if you multiply together the number of trees encountered on each of the listed slopes?") {
+                assertEquals(3898725600, day03.part2(DayInput().lines(day03)))
             }
         }
     }
