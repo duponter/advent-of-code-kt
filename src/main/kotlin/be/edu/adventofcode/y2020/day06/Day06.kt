8ae7013ed4616b2648979870e53788018752aa34
Erwin Dupont <erwin.dupont@aca-it.be>
2020-12-06 10:34:13 +0100

    [2020 - Day 06] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2020/day06/Day06.kt
+++ src/main/kotlin/be/edu/adventofcode/y2020/day06/Day06.kt
@@ -1,13 +1,16 @@
 package be.edu.adventofcode.y2020.day06
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day06 {
-    fun part1(input: Lines): Int {
-        return input.get().count()
+    fun part1(input: Text): Int {
+        return input.get().split("\n\n")
+            .map { it.filter { ch -> ch != '\n' } }
+            .map { it.chars().distinct().count() }
+            .sum().toInt()
     }
 
-    fun part2(input: Lines): Int {
+    fun part2(input: Text): Int {
         return input.get().count()
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2020/day06/Day06Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2020/day06/Day06Spec.kt
@@ -1,7 +1,7 @@
 package be.edu.adventofcode.y2020.day06
 
 import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertEquals
@@ -10,19 +10,19 @@ object Day06Spec : Spek({
     describe("day 06") {
         val day06 = Day06()
         context("part 1") {
-            it("testcase") {
-                assertEquals(1, day06.part1(LinesFromArray("line")))
+            it("Answered questions") {
+                assertEquals(11, day06.part1(TextFromString("abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb")))
             }
-            it("should answer Part1") {
-                assertEquals(0, day06.part1(DayInput().lines(day06)))
+            it("Part 1 - What is the sum of those counts?") {
+                assertEquals(6532, day06.part1(DayInput().text(day06)))
             }
         }
-        context("part 2") {
+        xcontext("part 2") {
             it("testcase") {
-                assertEquals(1, day06.part2(LinesFromArray("line")))
+                assertEquals(1, day06.part2(TextFromString("line")))
             }
             it("should answer Part2") {
-                assertEquals(0, day06.part2(DayInput().lines(day06)))
+                assertEquals(0, day06.part2(DayInput().text(day06)))
             }
         }
     }
