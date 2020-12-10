678609fe2c3e0689374d27ad2e235a10244b0baf
Erwin Dupont <erwin.dupont@aca-it.be>
2020-12-10 09:43:32 +0100

    [2020 - Day 10] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2020/day10/Day10.kt
+++ src/main/kotlin/be/edu/adventofcode/y2020/day10/Day10.kt
@@ -4,7 +4,15 @@ import be.edu.adventofcode.Lines
 
 class Day10 {
     fun part1(input: Lines): Int {
-        return input.get().count()
+        val outputJoltages = input.get().map { it.toInt() }
+        val joltDifferenceCount: Map<Int, Int> = (listOf(0, outputJoltages.maxOrNull()!! + 3) + outputJoltages)
+            .sorted()
+            .windowed(2)
+            .map { it[1] - it[0] }
+            .groupingBy { it }
+            .eachCount()
+
+        return joltDifferenceCount[1]!! * joltDifferenceCount[3]!!
     }
 
     fun part2(input: Lines): Int {
--- src/test/kotlin/be/edu/adventofcode/y2020/day10/Day10Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2020/day10/Day10Spec.kt
@@ -10,14 +10,17 @@ object Day10Spec : Spek({
     describe("day 10") {
         val day10 = Day10()
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                assertEquals(1, day10.part1(LinesFromArray("line")))
+            it("Part 1 - Testcase 1 - What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?") {
+                assertEquals(35, day10.part1(LinesFromArray("16", "10", "15", "5", "1", "11", "7", "19", "6", "12", "4")))
             }
-            it("Part 1 - Solution - ") {
-                assertEquals(0, day10.part1(DayInput().lines(day10)))
+            it("Part 1 - Testcase 2 - What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?") {
+                assertEquals(220, day10.part1(LinesFromArray("28", "33", "18", "42", "31", "14", "46", "20", "48", "47", "24", "23", "49", "45", "19", "38", "39", "11", "1", "32", "25", "35", "8", "17", "7", "9", "4", "2", "34", "10", "3")))
+            }
+            it("Part 1 - Solution - What is the number of 1-jolt differences multiplied by the number of 3-jolt differences?") {
+                assertEquals(2470, day10.part1(DayInput().lines(day10)))
             }
         }
-        context("part 2") {
+        xcontext("part 2") {
             it("Part 2 - Testcase - ") {
                 assertEquals(1, day10.part2(LinesFromArray("line")))
             }
