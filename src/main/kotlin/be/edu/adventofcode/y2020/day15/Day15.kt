2076eb58b06d8ae445aadbfc76dd5cdfc5579372
Erwin Dupont <erwin.dupont@aca-it.be>
2020-12-15 14:17:15 +0100

    [2020 - Day 15] Solved part 2

--- src/main/kotlin/be/edu/adventofcode/y2020/day15/Day15.kt
+++ src/main/kotlin/be/edu/adventofcode/y2020/day15/Day15.kt
@@ -3,11 +3,15 @@ package be.edu.adventofcode.y2020.day15
 import be.edu.adventofcode.Text
 
 class Day15 {
-    fun part1(input: Text): Int {
+    fun part1(input: Text): Int = memoryGame(input, 2020)
+
+    fun part2(input: Text): Int = memoryGame(input, 30000000)
+
+    private fun memoryGame(input: Text, targetNumber: Int): Int {
         val turns = input.get().split(',').mapIndexed { index, number -> number.toInt() to index + 1 }
         val (startValue, startTurn) = turns.last()
         val memory = turns.dropLast(1).associate { it }.toMutableMap()
-        return (startTurn + 1..2020).fold(startValue) { prevValue, currentTurn -> playMemoryGame(memory, prevValue, currentTurn) }
+        return (startTurn + 1..targetNumber).fold(startValue) { prevValue, currentTurn -> playMemoryGame(memory, prevValue, currentTurn) }
     }
 
     private fun playMemoryGame(memory: MutableMap<Int, Int>, prevValue: Int, currentTurn: Int): Int =
@@ -19,8 +23,4 @@ class Day15 {
             memory[prevValue] = currentTurn - 1
             0
         }
-
-    fun part2(input: Text): Int {
-        return input.get().count()
-    }
 }
--- src/test/kotlin/be/edu/adventofcode/y2020/day15/Day15Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2020/day15/Day15Spec.kt
@@ -34,11 +34,29 @@ object Day15Spec : Spek({
                 assertEquals(1373, day15.part1(TextFromString("0,1,5,10,3,12,19")))
             }
         }
-        xcontext("part 2") {
-            it("Part 2 - Testcase - ") {
-                assertEquals(1, day15.part2(TextFromString("")))
+        context("part 2") {
+            it("Part 2 - Testcase 1 - What will be the 30000000th number spoken?") {
+                assertEquals(175594, day15.part2(TextFromString("0,3,6")))
             }
-            it("Part 2 - Solution - ") {
+            it("Part 2 - Testcase 2 - What will be the 30000000th number spoken?") {
+                assertEquals(2578, day15.part2(TextFromString("1,3,2")))
+            }
+            it("Part 2 - Testcase 3 - What will be the 30000000th number spoken?") {
+                assertEquals(3544142, day15.part2(TextFromString("2,1,3")))
+            }
+            it("Part 2 - Testcase 4 - What will be the 30000000th number spoken?") {
+                assertEquals(261214, day15.part2(TextFromString("1,2,3")))
+            }
+            it("Part 2 - Testcase 5 - What will be the 30000000th number spoken?") {
+                assertEquals(6895259, day15.part2(TextFromString("2,3,1")))
+            }
+            it("Part 2 - Testcase 6 - What will be the 30000000th number spoken?") {
+                assertEquals(18, day15.part2(TextFromString("3,2,1")))
+            }
+            it("Part 2 - Testcase 7 - What will be the 30000000th number spoken?") {
+                assertEquals(362, day15.part2(TextFromString("3,1,2")))
+            }
+            it("Part 2 - Solution - What will be the 30000000th number spoken?") {
                 assertEquals(0, day15.part2(TextFromString("0,1,5,10,3,12,19")))
             }
         }
