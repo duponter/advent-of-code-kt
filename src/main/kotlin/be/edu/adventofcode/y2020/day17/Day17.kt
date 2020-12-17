7ab495b7bd1c6420b1c2bf51453b12bef56abfbb
Erwin Dupont <erwin.dupont@aca-it.be>
2020-12-17 08:06:38 +0100

    [2020 - Day 17] Generation

---
+++ src/main/kotlin/be/edu/adventofcode/y2020/day17/Day17.kt
@@ -0,0 +1,13 @@
+package be.edu.adventofcode.y2020.day17
+
+import be.edu.adventofcode.Lines
+
+class Day17 {
+    fun part1(input: Lines): Int {
+        return input.get().count()
+    }
+
+    fun part2(input: Lines): Int {
+        return input.get().count()
+    }
+}
