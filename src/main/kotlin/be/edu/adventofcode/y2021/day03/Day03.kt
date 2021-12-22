4994c4091a7c198eb389e4cabb6ff152f72f2d48
Erwin Dupont <erwin.dupont@aca-it.be>
2021-12-22 17:25:07 +0100

    [2021 - Day 03] Solved part 2

--- src/main/kotlin/be/edu/adventofcode/y2021/day03/Day03.kt
+++ src/main/kotlin/be/edu/adventofcode/y2021/day03/Day03.kt
@@ -14,7 +14,19 @@ class Day03 {
         return gammaRate.toDecimal() * gammaRate.inv().toDecimal()
     }
 
-    fun part2(input: Lines): Int {
-        return input.get().count()
+    fun part2(input: Lines): Long {
+        val oxygenGeneratorRating = BinaryString(filter(input.get(), { ones: List<String>, zeroes: List<String> -> if (ones.size >= zeroes.size) ones else zeroes }, 0))
+        val co2ScrubberRating = BinaryString(filter(input.get(), { ones: List<String>, zeroes: List<String> -> if (ones.size >= zeroes.size) zeroes else ones }, 0))
+        return oxygenGeneratorRating.toDecimal() * co2ScrubberRating.toDecimal()
+    }
+
+    tailrec fun filter(binaryNumbers: List<String>, selector: (List<String>, List<String>) -> List<String>, position: Int): String {
+        val (ones, zeroes) = binaryNumbers.partition { it[position] == '1' }
+        val keep: List<String> = selector.invoke(ones, zeroes)
+        return if (keep.size == 1) {
+            keep.first()
+        } else {
+            filter(keep, selector, position + 1)
+        }
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2021/day03/Day03Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2021/day03/Day03Spec.kt
@@ -17,12 +17,12 @@ object Day03Spec : Spek({
                 assertEquals(3687446, day03.part1(DayInput().lines(day03)))
             }
         }
-        xcontext("part 2") {
-            it("Part 2 - Testcase - ") {
-                assertEquals(1, day03.part2(LinesFromArray("line")))
+        context("part 2") {
+            it("Part 2 - Testcase - What is the life support rating of the submarine?") {
+                assertEquals(230, day03.part2(LinesFromArray("00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010")))
             }
-            it("Part 2 - Solution - ") {
-                assertEquals(0, day03.part2(DayInput().lines(day03)))
+            it("Part 2 - Solution - What is the life support rating of the submarine?") {
+                assertEquals(4406844, day03.part2(DayInput().lines(day03)))
             }
         }
     }
