022014287816ec3b65febbbc4ccceee8c58bc6a7
Erwin Dupont <erwin.dupont@aca-it.be>
2021-12-22 14:04:43 +0100

    [2021 - Day 02] Generation

---
+++ src/test/kotlin/be/edu/adventofcode/y2021/day02/Day02Spec.kt
@@ -0,0 +1,29 @@
+package be.edu.adventofcode.y2021.day02
+
+import be.edu.adventofcode.DayInput
+import be.edu.adventofcode.LinesFromArray
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
+
+object Day02Spec : Spek({
+    describe("day 02") {
+        val day02 = Day02()
+        context("part 1") {
+            it("Part 1 - Testcase - ") {
+                assertEquals(1, day02.part1(LinesFromArray("line")))
+            }
+            it("Part 1 - Solution - ") {
+                assertEquals(0, day02.part1(DayInput().lines(day02)))
+            }
+        }
+        xcontext("part 2") {
+            it("Part 2 - Testcase - ") {
+                assertEquals(1, day02.part2(LinesFromArray("line")))
+            }
+            it("Part 2 - Solution - ") {
+                assertEquals(0, day02.part2(DayInput().lines(day02)))
+            }
+        }
+    }
+})
