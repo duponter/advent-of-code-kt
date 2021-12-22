8800993c21a83148f2c9505559b8cff25feba0b2
Erwin Dupont <erwin.dupont@aca-it.be>
2021-12-22 13:58:49 +0100

    [2021 - Day 01] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2021/day01/Day01.kt
+++ src/main/kotlin/be/edu/adventofcode/y2021/day01/Day01.kt
@@ -4,7 +4,10 @@ import be.edu.adventofcode.Lines
 
 class Day01 {
     fun part1(input: Lines): Int {
-        return input.get().count()
+        return input.get()
+            .map { it.toInt() }
+            .zipWithNext { f, s -> if (s > f) 1 else 0 }
+            .sum()
     }
 
     fun part2(input: Lines): Int {
