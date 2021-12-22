5d64a0b652634444420d29bece923bd84d348897
Erwin Dupont <erwin.dupont@aca-it.be>
2021-12-22 21:28:29 +0100

    [2021 - Day 04] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2021/day04/Day04.kt
+++ src/main/kotlin/be/edu/adventofcode/y2021/day04/Day04.kt
@@ -1,13 +1,71 @@
 package be.edu.adventofcode.y2021.day04
 
 import be.edu.adventofcode.Lines
+import be.edu.adventofcode.grid.Matrix
+import be.edu.adventofcode.grid.transpose
 
 class Day04 {
     fun part1(input: Lines): Int {
-        return input.get().count()
+        val lines: List<String> = input.get()
+        val drawnNumbers = lines.first()
+        val bingo = Bingo.parse(lines.drop(1), 5)
+        return drawnNumbers.split(',')
+            .map { it.toInt() }
+            .fold(-1) { acc, called -> if (acc > -1) acc else bingo.draw(called) }
     }
 
     fun part2(input: Lines): Int {
         return input.get().count()
     }
 }
+
+data class Bingo(val boards: List<Board>) {
+    companion object Parse {
+        fun parse(lines: List<String>, size: Int): Bingo {
+            return lines.chunked(size + 1) { it.drop(1).map { l -> l.trim().split(Regex(" +")).map { v -> Number(v.toInt()) } } }
+                .map { Board(it) }.let { Bingo(it) }
+        }
+    }
+
+    fun draw(called: Int): Int {
+        return boards.fold(-1) { acc, board -> if (acc > -1) acc else board.draw(called) }
+    }
+
+    fun print(): String {
+        return boards.joinToString("\n\n") { it.print() }
+    }
+}
+
+data class Board(val numbers: Matrix<Number>) {
+    fun draw(called: Int): Int {
+        val marked: Number? = numbers.flatten().firstOrNull { it.draw(called) }
+        return if (marked != null && wins()) score(called) else -1
+    }
+
+    private fun score(called: Int): Int {
+        return numbers.flatten().filterNot { it.marked }.sumOf { it.value } * called
+    }
+
+    private fun wins(): Boolean {
+        val winningRow: Boolean = numbers.any { it.all { n -> n.marked } }
+        val winningCol: Boolean = numbers.transpose().any { it.all { n -> n.marked } }
+        return (winningRow || winningCol)
+    }
+
+    fun print(): String {
+        return numbers.joinToString("\n") { it.joinToString(" - ") { n -> n.print() } }
+    }
+}
+
+data class Number(val value: Int, var marked: Boolean = false) {
+    fun draw(called: Int): Boolean {
+        marked = marked || value == called
+        return value == called
+    }
+
+    fun print(): String {
+        val formatted = String.format("%2d", value)
+        return if (marked) "($formatted)" else " $formatted "
+    }
+}
+
--- src/test/kotlin/be/edu/adventofcode/y2021/day04/Day04Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2021/day04/Day04Spec.kt
@@ -10,18 +10,66 @@ object Day04Spec : Spek({
     describe("day 04") {
         val day04 = Day04()
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                assertEquals(1, day04.part1(LinesFromArray("line")))
+            it("Part 1 - Testcase - What will your final score be if you choose that board?") {
+                assertEquals(
+                    4512, day04.part1(
+                        LinesFromArray(
+                            "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+                            "",
+                            "22 13 17 11  0",
+                            "8  2 23  4 24",
+                            "21  9 14 16  7",
+                            "6 10  3 18  5",
+                            "1 12 20 15 19",
+                            "",
+                            "3 15  0  2 22",
+                            "9 18 13 17  5",
+                            "19  8  7 25 23",
+                            "20 11 10 24  4",
+                            "14 21 16 12  6",
+                            "",
+                            "14 21 17 24  4",
+                            "10 16 15  9 19",
+                            "18  8 23 26 20",
+                            "22 11 13  6  5",
+                            "2  0 12  3  7"
+                        )
+                    )
+                )
             }
-            it("Part 1 - Solution - ") {
-                assertEquals(0, day04.part1(DayInput().lines(day04)))
+            it("Part 1 - Solution - What will your final score be if you choose that board?") {
+                assertEquals(67716, day04.part1(DayInput().lines(day04)))
             }
         }
         xcontext("part 2") {
-            it("Part 2 - Testcase - ") {
-                assertEquals(1, day04.part2(LinesFromArray("line")))
+            it("Part 2 - Testcase - Once it wins, what would its final score be?") {
+                assertEquals(
+                    1924, day04.part2(
+                        LinesFromArray(
+                            "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+                            "",
+                            "22 13 17 11  0",
+                            "8  2 23  4 24",
+                            "21  9 14 16  7",
+                            "6 10  3 18  5",
+                            "1 12 20 15 19",
+                            "",
+                            "3 15  0  2 22",
+                            "9 18 13 17  5",
+                            "19  8  7 25 23",
+                            "20 11 10 24  4",
+                            "14 21 16 12  6",
+                            "",
+                            "14 21 17 24  4",
+                            "10 16 15  9 19",
+                            "18  8 23 26 20",
+                            "22 11 13  6  5",
+                            "2  0 12  3  7"
+                        )
+                    )
+                )
             }
-            it("Part 2 - Solution - ") {
+            it("Part 2 - Solution - Once it wins, what would its final score be?") {
                 assertEquals(0, day04.part2(DayInput().lines(day04)))
             }
         }
