8958ef8e7670920224a434cf7642f4e227ab4f74
Erwin Dupont <erwin.dupont@aca-it.be>
2021-12-24 16:19:41 +0100

    [2021 - Day 07] Generation

---
+++ src/main/kotlin/be/edu/adventofcode/y2021/day07/Day07.kt
@@ -0,0 +1,13 @@
+package be.edu.adventofcode.y2021.day07
+
+import be.edu.adventofcode.Text
+
+class Day07 {
+    fun part1(input: Text): Int {
+        return input.get().count()
+    }
+
+    fun part2(input: Text): Int {
+        return input.get().count()
+    }
+}
---
+++ src/test/kotlin/be/edu/adventofcode/y2021/day07/Day07Spec.kt
@@ -0,0 +1,29 @@
+package be.edu.adventofcode.y2021.day07
+
+import be.edu.adventofcode.DayInput
+import be.edu.adventofcode.TextFromString
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertEquals
+
+object Day07Spec : Spek({
+    describe("day 07") {
+        val day07 = Day07()
+        context("part 1") {
+            it("Part 1 - Testcase - How much fuel must they spend to align to that position?") {
+                assertEquals(37, day07.part1(TextFromString("16,1,2,0,4,2,7,1,2,14")))
+            }
+            it("Part 1 - Solution - How much fuel must they spend to align to that position?") {
+                assertEquals(0, day07.part1(DayInput().text(day07)))
+            }
+        }
+        xcontext("part 2") {
+            it("Part 2 - Testcase - ") {
+                assertEquals(1, day07.part2(TextFromString("line")))
+            }
+            it("Part 2 - Solution - ") {
+                assertEquals(0, day07.part2(DayInput().text(day07)))
+            }
+        }
+    }
+})
