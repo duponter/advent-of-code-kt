8e0a58617470f24f0af8e13cc060fff6d1efbf90
Erwin Dupont <erwin.dupont@aca-it.be>
2022-11-29 08:29:47 +0100

    [2022 - Day 01] Generation

---
+++ src/main/kotlin/be/edu/adventofcode/y2022/day01/Day01.kt
@@ -0,0 +1,22 @@
+package be.edu.adventofcode.y2022.day01
+
+import be.edu.adventofcode.Lines
+import be.edu.adventofcode.LinesFromArray
+
+class Day01 {
+    fun part1(input: Lines): Int {
+        return input.get().count()
+    }
+
+    fun part2(input: Lines): Int {
+        return input.get().count()
+    }
+}
+
+fun main() {
+    val testInput = LinesFromArray(
+
+    )
+    println("Day 01, part 1 = ${Day01().part1(testInput)}")
+//    println("Day 01, part 2 = ${Day01().part2(testInput)}")
+}
