9c2e18d833f42bad78b6d65332f005b843af2d07
Erwin Dupont <erwin.dupont@aca-it.be>
2022-12-05 10:14:37 +0100

    [2022 - Day 05] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2022/Day05.kt
+++ src/main/kotlin/be/edu/adventofcode/y2022/Day05.kt
@@ -1,12 +1,49 @@
 package be.edu.adventofcode.y2022
 
 import be.edu.adventofcode.Lines
+import be.edu.adventofcode.StringDestructure
+import be.edu.adventofcode.grid.transpose
+import be.edu.adventofcode.y2017.day10.split
 
 class Day05 {
-    fun part1(input: Lines): Int {
-        return input.get()
-            .count()
+    fun part1(input: Lines): String {
+        val lines = input.get()
+        val split = lines.split(lines.indexOf(""))
+
+        val maxLineLength = split.first.dropLast(1).maxOf { it.length }
+        val stacks: List<ArrayDeque<String>> = split.first.dropLast(1)
+            .map { it.padEnd(maxLineLength, ' ') }
+            .map { it.chunked(1) }
+            .transpose()
+            .filterIndexed { index, _ -> (index - 1) % 4 == 0 }
+            .map { it.filter { crate -> crate.isNotBlank() } }
+            .map { ArrayDeque(it) }
+
+        val arranged = split.second.drop(1)
+            .map { Instruction.parse(it) }
+            .fold(stacks) { stack, instruction -> instruction.execute(stack) }
+
+        return arranged.joinToString("") { it.first() }
     }
+
+    data class Instruction(val count: Int, val src: Int, val dest: Int) {
+        companion object {
+            fun parse(input: String): Instruction {
+                val destructured = StringDestructure("move (\\d+) from (\\d+) to (\\d+)").triple(input)
+                return Instruction(destructured.first.toInt(), destructured.second.toInt(), destructured.third.toInt())
+            }
+        }
+
+        fun execute(stacks: List<ArrayDeque<String>>): List<ArrayDeque<String>> {
+            val sourceStack = stacks[src - 1]
+            val destinationStack = stacks[dest - 1]
+            repeat(count) {
+                destinationStack.addFirst(sourceStack.removeFirst())
+            }
+            return stacks
+        }
+    }
+
 
     fun part2(input: Lines): Int {
         return input.get()
--- src/test/kotlin/be/edu/adventofcode/y2022/Day05Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2022/Day05Spec.kt
@@ -9,14 +9,22 @@ class Day05Spec : DescribeSpec({
     describe("day 05") {
         val day05 = Day05()
         val testInput = LinesFromArray(
-            ""
+            "    [D]",
+            "[N] [C]",
+            "[Z] [M] [P]",
+            " 1   2   3 ",
+            "",
+            "move 1 from 2 to 1",
+            "move 3 from 1 to 3",
+            "move 2 from 2 to 1",
+            "move 1 from 1 to 2"
         )
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                day05.part1(testInput) shouldBe 1
+            it("Part 1 - Testcase - After the rearrangement procedure completes, what crate ends up on top of each stack?") {
+                day05.part1(testInput) shouldBe "CMZ"
             }
-            it("Part 1 - Solution - ") {
-                day05.part1(DayInput().lines(day05)) shouldBe 0
+            it("Part 1 - Solution - After the rearrangement procedure completes, what crate ends up on top of each stack?") {
+                day05.part1(DayInput().lines(day05)) shouldBe "TWSGQHNHL"
             }
         }
         xcontext("part 2") {
