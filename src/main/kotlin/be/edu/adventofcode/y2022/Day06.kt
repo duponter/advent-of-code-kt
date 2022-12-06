29f2c098e249cce1231edfe77894700f56dde562
Erwin Dupont <erwin.dupont@aca-it.be>
2022-12-06 13:51:45 +0100

    [2022 - Day 06] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2022/Day06.kt
+++ src/main/kotlin/be/edu/adventofcode/y2022/Day06.kt
@@ -1,11 +1,13 @@
 package be.edu.adventofcode.y2022
 
 import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day06 {
-    fun part1(input: Lines): Int {
-        return input.get()
-            .count()
+    fun part1(input: Text): Int {
+        val datastream = input.get()
+        val marker = datastream.windowedSequence(4).toList().first { it.toSet().size == 4 }
+        return datastream.length - datastream.substringAfter(marker).length
     }
 
     fun part2(input: Lines): Int {
--- src/test/kotlin/be/edu/adventofcode/y2022/Day06Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2022/Day06Spec.kt
@@ -2,6 +2,7 @@ package be.edu.adventofcode.y2022
 
 import be.edu.adventofcode.DayInput
 import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
@@ -12,11 +13,15 @@ class Day06Spec : DescribeSpec({
             ""
         )
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                day06.part1(testInput) shouldBe 0
+            it("Part 1 - Testcase - How many characters need to be processed before the first start-of-packet marker is detected?") {
+                day06.part1(TextFromString("mjqjpqmgbljsphdztnvjfqwrcgsmlb")) shouldBe 7
+                day06.part1(TextFromString("bvwbjplbgvbhsrlpgdmjqwftvncz")) shouldBe 5
+                day06.part1(TextFromString("nppdvjthqldpwncqszvftbrmjlhg")) shouldBe 6
+                day06.part1(TextFromString("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")) shouldBe 10
+                day06.part1(TextFromString("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")) shouldBe 11
             }
-            it("Part 1 - Solution - ") {
-                day06.part1(DayInput().lines(day06)) shouldBe 0
+            it("Part 1 - Solution - How many characters need to be processed before the first start-of-packet marker is detected?") {
+                day06.part1(DayInput().text(day06)) shouldBe 1757
             }
         }
         xcontext("part 2") {
