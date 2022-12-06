cbf6c590bafb84945bb85f967989f40ecbf4a964
Erwin Dupont <erwin.dupont@aca-it.be>
2022-12-06 13:56:28 +0100

    [2022 - Day 06] Solved part 2

--- src/main/kotlin/be/edu/adventofcode/y2022/Day06.kt
+++ src/main/kotlin/be/edu/adventofcode/y2022/Day06.kt
@@ -1,17 +1,14 @@
 package be.edu.adventofcode.y2022
 
-import be.edu.adventofcode.Lines
 import be.edu.adventofcode.Text
 
 class Day06 {
-    fun part1(input: Text): Int {
-        val datastream = input.get()
-        val marker = datastream.windowedSequence(4).toList().first { it.toSet().size == 4 }
-        return datastream.length - datastream.substringAfter(marker).length
-    }
+    fun part1(input: Text): Int = indexAfterMarkerOfCharCount(input.get(), 4)
 
-    fun part2(input: Lines): Int {
-        return input.get()
-            .count()
+    fun part2(input: Text): Int = indexAfterMarkerOfCharCount(input.get(), 14)
+
+    private fun indexAfterMarkerOfCharCount(datastream: String, count: Int): Int {
+        val marker = datastream.windowedSequence(count).toList().first { it.toSet().size == count }
+        return datastream.length - datastream.substringAfter(marker).length
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2022/Day06Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2022/Day06Spec.kt
@@ -1,7 +1,6 @@
 package be.edu.adventofcode.y2022
 
 import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
 import be.edu.adventofcode.TextFromString
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
@@ -9,9 +8,6 @@ import io.kotest.matchers.shouldBe
 class Day06Spec : DescribeSpec({
     describe("day 06") {
         val day06 = Day06()
-        val testInput = LinesFromArray(
-            ""
-        )
         context("part 1") {
             it("Part 1 - Testcase - How many characters need to be processed before the first start-of-packet marker is detected?") {
                 day06.part1(TextFromString("mjqjpqmgbljsphdztnvjfqwrcgsmlb")) shouldBe 7
@@ -24,12 +20,16 @@ class Day06Spec : DescribeSpec({
                 day06.part1(DayInput().text(day06)) shouldBe 1757
             }
         }
-        xcontext("part 2") {
-            it("Part 2 - Testcase - ") {
-                day06.part2(testInput) shouldBe 0
+        context("part 2") {
+            it("Part 2 - Testcase - How many characters need to be processed before the first start-of-message marker is detected?") {
+                day06.part2(TextFromString("mjqjpqmgbljsphdztnvjfqwrcgsmlb")) shouldBe 19
+                day06.part2(TextFromString("bvwbjplbgvbhsrlpgdmjqwftvncz")) shouldBe 23
+                day06.part2(TextFromString("nppdvjthqldpwncqszvftbrmjlhg")) shouldBe 23
+                day06.part2(TextFromString("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg")) shouldBe 29
+                day06.part2(TextFromString("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw")) shouldBe 26
             }
-            it("Part 2 - Solution - ") {
-                day06.part2(DayInput().lines(day06)) shouldBe 0
+            it("Part 2 - Solution - How many characters need to be processed before the first start-of-message marker is detected?") {
+                day06.part2(DayInput().text(day06)) shouldBe 2950
             }
         }
     }
