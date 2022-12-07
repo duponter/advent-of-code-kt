1331a729eeea175e6b176870259e9abdc7677f80
Erwin Dupont <erwin.dupont@aca-it.be>
2022-12-07 15:52:07 +0100

    [2022 - Day 07] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2022/Day07.kt
+++ src/main/kotlin/be/edu/adventofcode/y2022/Day07.kt
@@ -3,13 +3,82 @@ package be.edu.adventofcode.y2022
 import be.edu.adventofcode.Lines
 
 class Day07 {
-    fun part1(input: Lines): Int {
-        return input.get()
-            .count()
+    fun part1(input: Lines): Long {
+        val instructions = input.get().map { Instruction.parse(it) }
+        instructions.fold(mutableListOf(Directory("/"))) { dirtree, instruction -> instruction.execute(dirtree) }
+        return instructions.filterIsInstance<Directory>().map { it.size() }.filter { it <= 100000 }.sumOf { it }
     }
 
     fun part2(input: Lines): Int {
         return input.get()
             .count()
+    }
+
+    interface Instruction {
+        companion object {
+            fun parse(input: String): Instruction {
+                if (input == "$ ls") return ListContent()
+                if (input.startsWith("$ cd")) return ChangeDirectory(input.removePrefix("$ cd "))
+                if (input.startsWith("dir")) return Directory(input.removePrefix("dir "))
+                else {
+                    val tokens = input.split(' ')
+                    return File(tokens.last(), tokens.first().toLong())
+                }
+            }
+        }
+
+        fun execute(dirtree: MutableList<Directory>): MutableList<Directory>
+    }
+
+    data class ChangeDirectory(val arg: String) : Instruction {
+        override fun execute(dirtree: MutableList<Directory>): MutableList<Directory> {
+            if (arg == "/") {
+                return mutableListOf(dirtree.first())
+            }
+            if (arg == "..") {
+                dirtree.removeLast()
+            } else {
+                dirtree.add(dirtree.last().findDirectory(arg))
+            }
+            return dirtree
+        }
+    }
+
+    class ListContent() : Instruction {
+        override fun execute(dirtree: MutableList<Directory>): MutableList<Directory> {
+            return dirtree
+        }
+    }
+
+    interface FileSystem : Instruction {
+        fun name(): String
+
+        fun size(): Long
+
+        override fun execute(dirtree: MutableList<Directory>): MutableList<Directory> {
+            dirtree.last().addContent(this)
+            return dirtree
+        }
+    }
+
+    data class File(val name: String, val size: Long) : FileSystem {
+        override fun name(): String = name
+
+        override fun size(): Long = size
+    }
+
+    data class Directory(val name: String, val content: MutableList<FileSystem> = mutableListOf()) : FileSystem {
+        override fun name(): String = name
+
+        override fun size(): Long = content.sumOf { it.size() }
+
+        fun addContent(toAdd: FileSystem): Directory {
+            content.add(toAdd)
+            return this;
+        }
+
+        fun findDirectory(name: String): Directory {
+            return content.filterIsInstance<Directory>().first { it.name() == name }
+        }
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2022/Day07Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2022/Day07Spec.kt
@@ -9,14 +9,36 @@ class Day07Spec : DescribeSpec({
     describe("day 07") {
         val day07 = Day07()
         val testInput = LinesFromArray(
-            ""
+            "$ cd /",
+            "$ ls",
+            "dir a",
+            "14848514 b.txt",
+            "8504156 c.dat",
+            "dir d",
+            "$ cd a",
+            "$ ls",
+            "dir e",
+            "29116 f",
+            "2557 g",
+            "62596 h.lst",
+            "$ cd e",
+            "$ ls",
+            "584 i",
+            "$ cd ..",
+            "$ cd ..",
+            "$ cd d",
+            "$ ls",
+            "4060174 j",
+            "8033020 d.log",
+            "5626152 d.ext",
+            "7214296 k"
         )
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                day07.part1(testInput) shouldBe 0
+            it("Part 1 - Testcase - What is the sum of the total sizes of directories with a total size of at most 100000?") {
+                day07.part1(testInput) shouldBe 95437
             }
-            it("Part 1 - Solution - ") {
-                day07.part1(DayInput().lines(day07)) shouldBe 0
+            it("Part 1 - Solution - What is the sum of the total sizes of directories with a total size of at most 100000?") {
+                day07.part1(DayInput().lines(day07)) shouldBe 1307902
             }
         }
         xcontext("part 2") {
