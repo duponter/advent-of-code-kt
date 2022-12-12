aa1cf9dc70edfd4a2b7999e786616fe02d7ee431
Erwin Dupont <erwin.dupont@aca-it.be>
2022-12-12 08:14:01 +0100

    [2022 - Day 11] Converted Int to BigInteger to prevent negative numbers

--- src/main/kotlin/be/edu/adventofcode/y2022/Day11.kt
+++ src/main/kotlin/be/edu/adventofcode/y2022/Day11.kt
@@ -1,6 +1,7 @@
 package be.edu.adventofcode.y2022
 
 import be.edu.adventofcode.Lines
+import java.math.BigInteger
 
 class Day11 {
     fun part1(input: Lines): Int {
@@ -16,59 +17,72 @@ class Day11 {
         repeat(10000) {
             monkeys.forEach { it.takeTurnNoRelief(monkeys) }
         }
+        monkeys.forEach { println(it.items) }
         val counts = monkeys.map { it.inspectionCount.count.toLong() }
+        counts.zip(
+            listOf(
+                "Monkey 0 inspected items 52166 times.",
+                "Monkey 1 inspected items 47830 times.",
+                "Monkey 2 inspected items 1938 times.",
+                "Monkey 3 inspected items 52013 times."
+            )
+        ) { a, e -> "Expected: $e Actual: $a" }.forEach { println(it) }
         return counts.sorted().takeLast(2).let { it.first() * it.last() }
     }
 
-    data class Monkey(val id: Int, val items: MutableList<Int>, val inspectOperation: (Int) -> Int, val testDivisor: Int, val nextMonkey: Pair<Int, Int>, val inspectionCount: InspectionCount = InspectionCount()) {
+    data class Monkey(val id: Int, val items: MutableList<BigInteger>, val inspectOperation: (BigInteger) -> BigInteger, val testDivisor: BigInteger, val nextMonkey: Pair<Int, Int>, val inspectionCount: InspectionCount = InspectionCount()) {
         companion object {
             fun parse(input: List<String>): Monkey {
                 val id = input[0].substringAfter("Monkey ").dropLast(1).toInt()
-                val items = input[1].trim().substringAfter("Starting items: ").split(", ").map { it.toInt() }.toMutableList()
+                val items = input[1].trim().substringAfter("Starting items: ").split(", ").map { it.toBigInteger() }.toMutableList()
                 val tempOperation = input[2].trim().substringAfter("Operation: new = old ")
                 val term = tempOperation.drop(2)
                 val inspectOperation =
-                    if (tempOperation == "* old") { x: Int -> x * x }
-                    else if (tempOperation.startsWith("+")) { x: Int -> x + term.toInt() }
-                    else if (tempOperation.startsWith("*")) { x: Int -> x * term.toInt() }
+                    if (tempOperation == "* old") { x: BigInteger -> x * x }
+                    else if (tempOperation.startsWith("+")) { x: BigInteger -> x + term.toBigInteger() }
+                    else if (tempOperation.startsWith("*")) { x: BigInteger -> x * term.toBigInteger() }
                     else throw IllegalArgumentException(tempOperation)
-                val testDivisor = input[3].substringAfterLast(' ').toInt()
+                val testDivisor = input[3].substringAfterLast(' ').toBigInteger()
                 val nextMonkey = input[4].substringAfterLast(' ').toInt() to input[5].substringAfterLast(' ').toInt()
                 return Monkey(id, items, inspectOperation, testDivisor, nextMonkey)
             }
 
             fun parseNoMultiply(input: List<String>): Monkey {
                 val id = input[0].substringAfter("Monkey ").dropLast(1).toInt()
-                val items = input[1].trim().substringAfter("Starting items: ").split(", ").map { it.toInt() }.toMutableList()
+                val items = input[1].trim().substringAfter("Starting items: ").split(", ").map { it.toBigInteger() }.toMutableList()
                 val tempOperation = input[2].trim().substringAfter("Operation: new = old ")
                 val term = tempOperation.drop(2)
                 val inspectOperation =
-                    if (tempOperation == "* old") { x: Int -> x }   // no need to multiply when checking modulus only
-                    else if (tempOperation.startsWith("+")) { x: Int -> x + term.toInt() }
-                    else if (tempOperation.startsWith("*")) { x: Int -> x }   // no need to multiply when checking modulus only
+                    if (tempOperation == "* old") { x: BigInteger -> x }   // no need to multiply when checking modulus only
+                    else if (tempOperation.startsWith("+")) { x: BigInteger -> x + term.toBigInteger() }
+                    else if (tempOperation.startsWith("*")) { x: BigInteger -> x }   // no need to multiply when checking modulus only
                     else throw IllegalArgumentException(tempOperation)
-                val testDivisor = input[3].substringAfterLast(' ').toInt()
+                val testDivisor = input[3].substringAfterLast(' ').toBigInteger()
                 val nextMonkey = input[4].substringAfterLast(' ').toInt() to input[5].substringAfterLast(' ').toInt()
                 return Monkey(id, items, inspectOperation, testDivisor, nextMonkey)
             }
         }
 
         fun takeTurn(monkeys: List<Monkey>) {
-            takeTurn(monkeys) { x -> inspectOperation(x) / 3 }
+            takeTurn(monkeys) { x -> inspectOperation(x) / BigInteger.valueOf(3) }
         }
 
         fun takeTurnNoRelief(monkeys: List<Monkey>) {
             takeTurn(monkeys, inspectOperation)
         }
 
-        private fun takeTurn(monkeys: List<Monkey>, inspectOperation: (Int) -> Int) {
+        private fun takeTurn(monkeys: List<Monkey>, inspectOperation: (BigInteger) -> BigInteger) {
             items.forEach { throwItem(monkeys, inspectOperation(it)) }
             inspectionCount.count += items.size
             items.clear()
         }
 
-        private fun throwItem(monkeys: List<Monkey>, worryLevel: Int) {
-            val targetMonkey = if (worryLevel % testDivisor == 0) nextMonkey.first else nextMonkey.second
+        private fun throwItem(monkeys: List<Monkey>, worryLevel: BigInteger) {
+            /*
+            Ik had gezien dat alle delers priemgetallen waren, dus heb ik voor de berekening modulo het product ervan gedaan.
+            (Grootste gemene deler zou nog iets correcter zijn geweest.)
+             */
+            val targetMonkey = if (worryLevel.mod(testDivisor) == BigInteger.ZERO) nextMonkey.first else nextMonkey.second
             monkeys[targetMonkey].items.add(worryLevel)
         }
 
