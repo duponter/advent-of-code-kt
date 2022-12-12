5fd43e975803fa799d4236f7ae53e0383bda2607
Erwin Dupont <erwin.dupont@aca-it.be>
2022-12-12 08:31:06 +0100

    [2022 - Day 12] Generation

---
+++ src/main/kotlin/be/edu/adventofcode/y2022/Day12.kt
@@ -0,0 +1,17 @@
+package be.edu.adventofcode.y2022
+
+import be.edu.adventofcode.Algorithm
+import be.edu.adventofcode.Lines
+
+@Algorithm("Dijkstra")
+class Day12 {
+    fun part1(input: Lines): Int {
+        return input.get()
+            .count()
+    }
+
+    fun part2(input: Lines): Int {
+        return input.get()
+            .count()
+    }
+}
