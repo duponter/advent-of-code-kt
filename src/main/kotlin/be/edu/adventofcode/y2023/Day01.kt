31b334097875e8012de611926c9dccf4f18ae958
Erwin Dupont <erwin.dupont@acagroup.be>
2023-12-03 20:21:27 +0100

    [2023 - Day 01] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2023/Day01.kt
+++ src/main/kotlin/be/edu/adventofcode/y2023/Day01.kt
@@ -4,8 +4,9 @@ import be.edu.adventofcode.Lines
 
 class Day01 {
     fun part1(input: Lines): Int {
-        return input.get()
-            .count()
+        return input.get().sumOf {
+            it.toCharArray().filter { c -> c.isDigit() }.let { d -> d.first().digitToInt() * 10 + d.last().digitToInt() }
+        }
     }
 
     fun part2(input: Lines): Int {
--- src/test/kotlin/be/edu/adventofcode/y2023/Day01Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2023/Day01Spec.kt
@@ -9,21 +9,33 @@ class Day01Spec : DescribeSpec({
     describe("day 01") {
         val day01 = Day01()
         val testInput = LinesFromArray(
-          ""
+            "1abc2",
+            "pqr3stu8vwx",
+            "a1b2c3d4e5f",
+            "treb7uchet"
         )
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                day01.part1(testInput) shouldBe 0
+            it("Part 1 - Testcase - What is the sum of all of the calibration values?") {
+                day01.part1(testInput) shouldBe 142
             }
-            it("Part 1 - Solution - ") {
-                day01.part1(DayInput().lines(day01)) shouldBe 0
+            it("Part 1 - Solution - What is the sum of all of the calibration values?") {
+                day01.part1(DayInput().lines(day01)) shouldBe 55712
             }
         }
+        val testInput2 = LinesFromArray(
+            "two1nine" +
+            "eightwothree" +
+            "abcone2threexyz" +
+            "xtwone3four" +
+            "4nineeightseven2" +
+            "zoneight234" +
+            "7pqrstsixteen"
+        )
         xcontext("part 2") {
-            it("Part 2 - Testcase - ") {
-                day01.part2(testInput) shouldBe 0
+            it("Part 2 - Testcase - What is the sum of all of the calibration values?") {
+                day01.part2(testInput2) shouldBe 281
             }
-            it("Part 2 - Solution - ") {
+            it("Part 2 - Solution - What is the sum of all of the calibration values?") {
                 day01.part2(DayInput().lines(day01)) shouldBe 0
             }
         }
