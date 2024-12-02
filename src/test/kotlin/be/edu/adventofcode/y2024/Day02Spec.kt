f846fad05fb6a6d73e66415a3a4d8481f72d17fd
Erwin Dupont <erwin.dupont+github@gmail.com>
2024-12-02 10:50:04 +0100

    [2024 - Day 02] Generation

---
+++ src/test/kotlin/be/edu/adventofcode/y2024/Day02Spec.kt
@@ -0,0 +1,31 @@
+package be.edu.adventofcode.y2024
+
+import be.edu.adventofcode.DayInput
+import be.edu.adventofcode.LinesFromArray
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+
+class Day02Spec : DescribeSpec({
+    describe("day 02") {
+        val day02 = Day02()
+        val testInput = LinesFromArray(
+          ""
+        )
+        context("part 1") {
+            it("Part 1 - Testcase - ") {
+                day02.part1(testInput) shouldBe 0
+            }
+            it("Part 1 - Solution - ") {
+                day02.part1(DayInput().lines(day02)) shouldBe 0
+            }
+        }
+        xcontext("part 2") {
+            it("Part 2 - Testcase - ") {
+                day02.part2(testInput) shouldBe 0
+            }
+            it("Part 2 - Solution - ") {
+                day02.part2(DayInput().lines(day02)) shouldBe 0
+            }
+        }
+    }
+})
