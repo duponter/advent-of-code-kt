876ddd6f8a4b87c9a20df34fd4ee91b6e38fac65
Erwin Dupont <erwin.dupont+github@gmail.com>
2024-12-02 18:54:22 +0100

    [2024 - Day 02] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2024/Day02.kt
+++ src/main/kotlin/be/edu/adventofcode/y2024/Day02.kt
@@ -5,11 +5,29 @@ import be.edu.adventofcode.Lines
 class Day02 {
     fun part1(input: Lines): Int {
         return input.get()
-            .count()
+            .map { Report.parse(it) }
+            .count { it.safe() }
     }
 
     fun part2(input: Lines): Int {
         return input.get()
             .count()
+    }
+
+    data class Report(val levels: List<Int>) {
+        companion object {
+            fun parse(input: String): Report {
+                return Report(input.split(' ').map { it.toInt() })
+            }
+        }
+
+        fun safe(): Boolean {
+            val diffs = levels.zipWithNext { a, b -> a - b }
+
+            val min = diffs.minOrNull()!!
+            val max = diffs.maxOrNull()!!
+
+            return (min >= 1 && max <= 3) || (min >= -3 && max <= -1)
+        }
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2024/Day02Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2024/Day02Spec.kt
@@ -9,14 +9,19 @@ class Day02Spec : DescribeSpec({
     describe("day 02") {
         val day02 = Day02()
         val testInput = LinesFromArray(
-          ""
+            "7 6 4 2 1",
+            "1 2 7 8 9",
+            "9 7 6 2 1",
+            "1 3 2 4 5",
+            "8 6 4 4 1",
+            "1 3 6 7 9"
         )
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                day02.part1(testInput) shouldBe 0
+            it("Part 1 - Testcase - How many reports are safe?") {
+                day02.part1(testInput) shouldBe 2
             }
-            it("Part 1 - Solution - ") {
-                day02.part1(DayInput().lines(day02)) shouldBe 0
+            it("Part 1 - Solution - How many reports are safe?") {
+                day02.part1(DayInput().lines(day02)) shouldBe 686
             }
         }
         xcontext("part 2") {
