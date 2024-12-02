0390628923db8bc1c1c0e7322ab2ce8f972d639a
Erwin Dupont <erwin.dupont+github@gmail.com>
2024-12-02 10:36:20 +0100

    [2024 - Day 01] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2024/Day01.kt
+++ src/main/kotlin/be/edu/adventofcode/y2024/Day01.kt
@@ -1,11 +1,15 @@
 package be.edu.adventofcode.y2024
 
 import be.edu.adventofcode.Lines
+import be.edu.adventofcode.StringDestructure
+import kotlin.math.abs
 
 class Day01 {
     fun part1(input: Lines): Int {
-        return input.get()
-            .count()
+        val tokens = input.get().map { StringDestructure("^(\\d+)\\s+(\\d+)\$").pair(it) }
+        return tokens.map { it.first.toInt() }.sorted()
+            .zip(tokens.map { it.second.toInt() }.sorted()) { f, s -> abs(f - s) }
+            .sum()
     }
 
     fun part2(input: Lines): Int {
--- src/test/kotlin/be/edu/adventofcode/y2024/Day01Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2024/Day01Spec.kt
@@ -9,14 +9,19 @@ class Day01Spec : DescribeSpec({
     describe("day 01") {
         val day01 = Day01()
         val testInput = LinesFromArray(
-          ""
+            "3   4",
+            "4   3",
+            "2   5",
+            "1   3",
+            "3   9",
+            "3   3"
         )
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                day01.part1(testInput) shouldBe 0
+            it("Part 1 - Testcase - What is the total distance between your lists?") {
+                day01.part1(testInput) shouldBe 11
             }
-            it("Part 1 - Solution - ") {
-                day01.part1(DayInput().lines(day01)) shouldBe 0
+            it("Part 1 - Solution - What is the total distance between your lists?") {
+                day01.part1(DayInput().lines(day01)) shouldBe 2756096
             }
         }
         xcontext("part 2") {
