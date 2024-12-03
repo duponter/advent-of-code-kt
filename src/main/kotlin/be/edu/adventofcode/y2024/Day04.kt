a00126309f58af65721653ba1e2dfbc63b3cd1bc
Erwin Dupont <erwin.dupont+github@gmail.com>
2024-12-03 11:30:59 +0100

    [2024 - Day 04] Generation

---
+++ src/main/kotlin/be/edu/adventofcode/y2024/Day04.kt
@@ -0,0 +1,15 @@
+package be.edu.adventofcode.y2024
+
+import be.edu.adventofcode.Lines
+
+class Day04 {
+    fun part1(input: Lines): Int {
+        return input.get()
+            .count()
+    }
+
+    fun part2(input: Lines): Int {
+        return input.get()
+            .count()
+    }
+}
