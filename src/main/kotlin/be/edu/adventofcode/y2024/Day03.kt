7d68a1a57e9598761b0d39231732171ca192697b
Erwin Dupont <erwin.dupont+github@gmail.com>
2024-12-03 11:30:21 +0100

    [2024 - Day 03] Solved part 2

--- src/main/kotlin/be/edu/adventofcode/y2024/Day03.kt
+++ src/main/kotlin/be/edu/adventofcode/y2024/Day03.kt
@@ -4,12 +4,18 @@ import be.edu.adventofcode.Text
 
 class Day03 {
     fun part1(input: Text): Int {
-        return "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex().findAll(input.get())
-            .sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
+        val text = input.get()
+        return sumMultiplications(text)
     }
 
     fun part2(input: Text): Int {
-        return input.get()
-            .count()
+        return input.get().split("(?=don't\\(\\)|do\\(\\))".toRegex())
+            .filter { !it.startsWith("don't()") }
+            .sumOf { sumMultiplications(it) }
+    }
+
+    private fun sumMultiplications(text: String): Int {
+        return "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex().findAll(text)
+            .sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
     }
 }
