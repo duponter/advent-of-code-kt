6ca4baa97ef3fa20c8607a9ce268c7265f45ae69
Erwin Dupont <erwin.dupont+github@gmail.com>
2024-12-03 09:44:52 +0100

    [2024 - Day 03] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2024/Day03.kt
+++ src/main/kotlin/be/edu/adventofcode/y2024/Day03.kt
@@ -1,14 +1,14 @@
 package be.edu.adventofcode.y2024
 
-import be.edu.adventofcode.Lines
+import be.edu.adventofcode.Text
 
 class Day03 {
-    fun part1(input: Lines): Int {
-        return input.get()
-            .count()
+    fun part1(input: Text): Int {
+        return "mul\\((\\d{1,3}),(\\d{1,3})\\)".toRegex().findAll(input.get())
+            .sumOf { it.groupValues[1].toInt() * it.groupValues[2].toInt() }
     }
 
-    fun part2(input: Lines): Int {
+    fun part2(input: Text): Int {
         return input.get()
             .count()
     }
--- src/test/kotlin/be/edu/adventofcode/y2024/Day03Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2024/Day03Spec.kt
@@ -1,22 +1,22 @@
 package be.edu.adventofcode.y2024
 
 import be.edu.adventofcode.DayInput
-import be.edu.adventofcode.LinesFromArray
+import be.edu.adventofcode.TextFromString
 import io.kotest.core.spec.style.DescribeSpec
 import io.kotest.matchers.shouldBe
 
 class Day03Spec : DescribeSpec({
     describe("day 03") {
         val day03 = Day03()
-        val testInput = LinesFromArray(
-          ""
+        val testInput = TextFromString(
+          "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
         )
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                day03.part1(testInput) shouldBe 0
+            it("Part 1 - Testcase - What do you get if you add up all of the results of the multiplications?") {
+                day03.part1(testInput) shouldBe 161
             }
-            it("Part 1 - Solution - ") {
-                day03.part1(DayInput().lines(day03)) shouldBe 0
+            it("Part 1 - Solution - What do you get if you add up all of the results of the multiplications?") {
+                day03.part1(DayInput().text(day03)) shouldBe 166630675
             }
         }
         xcontext("part 2") {
@@ -24,7 +24,7 @@ class Day03Spec : DescribeSpec({
                 day03.part2(testInput) shouldBe 0
             }
             it("Part 2 - Solution - ") {
-                day03.part2(DayInput().lines(day03)) shouldBe 0
+                day03.part2(DayInput().text(day03)) shouldBe 0
             }
         }
     }
