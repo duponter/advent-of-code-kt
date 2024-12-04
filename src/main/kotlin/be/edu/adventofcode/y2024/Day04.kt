c6eb8e6c0faf5da96fb4800062c9ad4b824e02e7
Erwin Dupont <erwin.dupont+github@gmail.com>
2024-12-04 22:42:05 +0100

    [2024 - Day 04] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2024/Day04.kt
+++ src/main/kotlin/be/edu/adventofcode/y2024/Day04.kt
@@ -4,12 +4,57 @@ import be.edu.adventofcode.Lines
 
 class Day04 {
     fun part1(input: Lines): Int {
-        return input.get()
-            .count()
+        val xmas = "XMAS".toRegex()
+        val samx = "SAMX".toRegex()
+
+        val multiView = MultiView(input.get())
+        return multiView.horizontal()
+            .plus(multiView.vertical())
+            .plus(multiView.diagonalUp())
+            .plus(multiView.diagonalDown())
+            .sumOf { xmas.findAll(it).count() + samx.findAll(it).count() }
     }
 
     fun part2(input: Lines): Int {
         return input.get()
             .count()
+    }
+
+    class MultiView(val lines: List<String>) {
+
+        fun horizontal(): List<String> = lines
+
+        fun vertical(): List<String> = transpose(lines)
+
+        fun diagonalDown(): List<String> {
+            return transpose(lines.indices.map { x -> lines[x].drop(x) + " ".repeat(x) }).map { it.trim() }.reversed()
+                .plus(
+                    transpose((1 until lines.size).map { y -> " ".repeat(lines[y].count() - y) + lines[y].take(y) }).map { it.trim() }
+                        .reversed()
+                        .filter { it.isNotBlank() }
+                )
+        }
+
+        fun diagonalUp(): List<String> {
+            return transpose(lines.indices.map { x -> " ".repeat(x) + lines[x].take(lines[x].count() - x) }
+                .reversed()).map { it.trim() }
+                .plus(
+                    transpose(
+                        lines.indices
+                        .map { y -> lines[y].drop(lines[y].count() - y) + " ".repeat(lines[y].count() - y) }
+                        .reversed())
+                        .filter { it.isNotBlank() }
+                        .map { it.trim() }
+                )
+
+        }
+
+        private fun transpose(strings: List<String>): List<String> {
+            return strings[0].indices.map { x ->
+                strings.indices
+                    .map { y -> strings[y][x] }
+                    .joinToString("")
+            }
+        }
     }
 }
--- src/test/kotlin/be/edu/adventofcode/y2024/Day04Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2024/Day04Spec.kt
@@ -9,14 +9,23 @@ class Day04Spec : DescribeSpec({
     describe("day 04") {
         val day04 = Day04()
         val testInput = LinesFromArray(
-          ""
+            "MMMSXXMASM",
+            "MSAMXMSMSA",
+            "AMXSXMAAMM",
+            "MSAMASMSMX",
+            "XMASAMXAMM",
+            "XXAMMXXAMA",
+            "SMSMSASXSS",
+            "SAXAMASAAA",
+            "MAMMMXMMMM",
+            "MXMXAXMASX"
         )
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                day04.part1(testInput) shouldBe 0
+            it("Part 1 - Testcase - How many times does XMAS appear?") {
+                day04.part1(testInput) shouldBe 18
             }
-            it("Part 1 - Solution - ") {
-                day04.part1(DayInput().lines(day04)) shouldBe 0
+            it("Part 1 - Solution - How many times does XMAS appear?") {
+                day04.part1(DayInput().lines(day04)) shouldBe 2514
             }
         }
         xcontext("part 2") {
