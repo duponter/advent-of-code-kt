b862ffd14211bb2280f8c6b4bd7c903256120287
Erwin Dupont <erwin.dupont+github@gmail.com>
2024-12-05 09:10:04 +0100

    [2024 - Day 05] Solved part 1

--- src/main/kotlin/be/edu/adventofcode/y2024/Day05.kt
+++ src/main/kotlin/be/edu/adventofcode/y2024/Day05.kt
@@ -4,8 +4,21 @@ import be.edu.adventofcode.Lines
 
 class Day05 {
     fun part1(input: Lines): Int {
-        return input.get()
-            .count()
+        // parse page orders
+        val pageOrders = input.get().takeWhile { it.isNotBlank() }
+            .map { it.split('|').map { s -> s.toInt() }.toSet() }
+
+        // parse updates
+        val updates = input.get().drop(pageOrders.size + 1)
+            .map { it.split(',').map { s -> s.toInt() } }
+
+        // select valid updates
+        return updates.filter {
+            pageOrders.all { po ->
+                val intersect = it intersect po
+                intersect.size < 2 || (intersect.first() == po.first() && intersect.last() == po.last())
+            }
+        }.sumOf { it[(it.size - 1) / 2] }
     }
 
     fun part2(input: Lines): Int {
--- src/test/kotlin/be/edu/adventofcode/y2024/Day05Spec.kt
+++ src/test/kotlin/be/edu/adventofcode/y2024/Day05Spec.kt
@@ -9,14 +9,41 @@ class Day05Spec : DescribeSpec({
     describe("day 05") {
         val day05 = Day05()
         val testInput = LinesFromArray(
-          ""
+            "47|53",
+            "97|13",
+            "97|61",
+            "97|47",
+            "75|29",
+            "61|13",
+            "75|53",
+            "29|13",
+            "97|29",
+            "53|29",
+            "61|53",
+            "97|53",
+            "61|29",
+            "47|13",
+            "75|47",
+            "97|75",
+            "47|61",
+            "75|61",
+            "47|29",
+            "75|13",
+            "53|13",
+            "",
+            "75,47,61,53,29",
+            "97,61,53,29,13",
+            "75,29,13",
+            "75,97,47,61,53",
+            "61,13,29",
+            "97,13,75,29,47"
         )
         context("part 1") {
-            it("Part 1 - Testcase - ") {
-                day05.part1(testInput) shouldBe 0
+            it("Part 1 - Testcase - What do you get if you add up the middle page number from those correctly-ordered updates?") {
+                day05.part1(testInput) shouldBe 143
             }
-            it("Part 1 - Solution - ") {
-                day05.part1(DayInput().lines(day05)) shouldBe 0
+            it("Part 1 - Solution - What do you get if you add up the middle page number from those correctly-ordered updates?") {
+                day05.part1(DayInput().lines(day05)) shouldBe 5091
             }
         }
         xcontext("part 2") {
